09e603e3debad259fb23a39d5eb1b01477ae7cb2
Peter Inglesby <peter.inglesby@gmail.com>
2013-06-03 12:48:14 +0100

    Change keyboard navigation
    We no longer have the concept of a selected row, which makes the
    implementation much easier.
    
    Fixes #6 and #8.

--- assets/js/opal.js
+++ assets/js/opal.js
@@ -1,16 +1,3 @@
-var columns = [
-	{name: 'location', multi: false},
-	{name: 'demographics', multi: false}, 
-	{name: 'diagnosis', multi: true},
-	{name: 'pastMedicalHistory', multi: true},
-	{name: 'travel', multi: true},
-//	{name: 'microbiology', multi: true}, TODO: awaiting details of structure
-	{name: 'antibiotics', multi: true},
-	{name: 'microbiologyComments', multi: true},
-	{name: 'plan', multi: false},
-	{name: 'discharge', multi: false},
-];
-
 function clone(obj) {
 	if (typeof obj == 'object') {
 		return $.extend(true, {}, obj);
@@ -32,13 +19,13 @@ function getKeys(obj) {
 var app = angular.module('opalApp', ['$strap.directives']);
 
 app.controller('TableCtrl', function($scope, $http) {
-	$scope.rows = [];
-	$scope.selectedColumnName = null;
+	var editing;
 
-	var rix = 0;
-	var cix = -1;
-	var iix = -1;
-	var editing = false;
+	$scope.rows = [];
+
+	$scope.rix = 0; // row index
+	$scope.cix = 0; // column index
+	$scope.iix = 0; // item index
 
 	$http.get('data/records.json').success(function(rows) {
 //		for (var rix = 0; rix < rows.length; rix++) {
@@ -49,8 +36,29 @@ app.controller('TableCtrl', function($scope, $http) {
 //			}
 //		}
 		$scope.rows = rows;
-		selectRow();
 	});
+
+	$scope.columns = [
+		{name: 'location', title: 'Location', multi: false},
+		{name: 'demographics', title: 'Demographics', multi: false}, 
+		{name: 'diagnosis', title: 'Diagnosis', multi: true},
+		{name: 'pastMedicalHistory', title: 'Past Medical History', multi: true},
+		{name: 'travel', title: 'Travel', multi: true},
+		//	{name: 'microbiology', title: '', multi: true}, TODO: awaiting details of structure
+		{name: 'antibiotics', title: 'Antibiotics', multi: true},
+		{name: 'microbiologyComments', title: 'Microbiology Comments', multi: true},
+		{name: 'plan', title: 'Plan', multi: false},
+		{name: 'discharge', title: 'Discharge', multi: false},
+	];
+
+	$scope.getCix = function(name) {
+		for (var cix = 0; cix < $scope.columns.length; cix++) {
+			if ($scope.columns[cix].name == name) {
+				return cix;
+			}
+		}
+		throw 'Unexpected column name: ' + name
+	};
 
 	$http.get('data/conditions.json').success(function(conditions) {
 		$scope.conditions = conditions;
@@ -68,65 +76,57 @@ app.controller('TableCtrl', function($scope, $http) {
 	$scope.routes = ['Oral', 'IV'];
 	$scope.travelReasones = ['Visiting Friends and Relatives', 'Business', 'Military', 'Holiday'];
 
-	function selectRow() {
-		$scope.rows[rix]['_selected'] = true;
-	};
+	function startEdit() {
+		var rix = $scope.rix;
+		var cix = $scope.cix;
+		var iix = $scope.iix;
+		var columnName = $scope.columns[cix].name;
 
-	function deselectRow() {
-		delete $scope.rows[rix]['_selected'];
-	};
-
-	function selectColumn() {
-		$scope.selectedColumnName = columns[cix]['name'];
-		if (columns[cix]['multi']) {
-			iix = 0;
-			selectItem();
-		} else {
-			iix = -1;
-		}
-	};
-
-	function deselectColumn(cir) {
-		$scope.selectedColumnName = null;
-	};
-
-	function selectItem() {
-		$scope.rows[rix][$scope.selectedColumnName][iix]['_selected'] = true;
-	};
-
-	function deselectItem() {
-		delete $scope.rows[rix][$scope.selectedColumnName][iix]['_selected'];
-	};
-
-	function doEdit() {
 		editing = true;
-		if (columns[cix]['multi']) {
-			$scope.editing = clone($scope.rows[rix][$scope.selectedColumnName][iix]);
+		if ($scope.columns[cix]['multi']) {
+			$scope.editing = clone($scope.rows[rix][columnName][iix]);
 		} else {
-			$scope.editing = clone($scope.rows[rix][$scope.selectedColumnName]);
+			$scope.editing = clone($scope.rows[rix][columnName]);
 		}
-		$('#' + $scope.selectedColumnName + '-modal').modal()
-		$('#' + $scope.selectedColumnName + '-modal').find('input').first().focus();
+		$('#' + columnName + '-modal').modal()
+		$('#' + columnName + '-modal').find('input').first().focus();
 	};
 
 	$scope.saveEdit = function() {
-		$('#' + $scope.selectedColumnName + '-modal').modal('hide')
+		var rix = $scope.rix;
+		var cix = $scope.cix;
+		var iix = $scope.iix;
+		var columnName = $scope.columns[cix].name;
+
+		$('#' + columnName + '-modal').modal('hide')
 		editing = false;
-		if (columns[cix]['multi']) {
+		if ($scope.columns[cix]['multi']) {
 			if ($scope.editing._new) {
 				delete $scope.editing._new;
-				$scope.rows[rix][$scope.selectedColumnName].push({'_new': true});
+				$scope.rows[rix][columnName].push({'_new': true});
 			}
 
-			$scope.rows[rix][$scope.selectedColumnName][iix] = clone($scope.editing);
+			$scope.rows[rix][columnName][iix] = clone($scope.editing);
 		} else {
-			$scope.rows[rix][$scope.selectedColumnName] = clone($scope.editing);
+			$scope.rows[rix][columnName] = clone($scope.editing);
 		}
 	};
 
 	$scope.cancelEdit = function() {
-		$('#' + $scope.selectedColumnName + '-modal').modal('hide')
+		var cix = $scope.cix;
+		var columnName = $scope.columns[cix].name;
+
+		$('#' + columnName + '-modal').modal('hide')
 		editing = false;
+	};
+
+	function getNumItems(rix, cix) {
+		var column = $scope.columns[cix];
+		if (column.multi) {
+			return $scope.rows[rix][column.name].length;
+		} else {
+			return 1;
+		}
 	};
 
 	$scope.keypress = function(e) {
@@ -134,77 +134,65 @@ app.controller('TableCtrl', function($scope, $http) {
 			switch (e.keyCode) {
 				case 27: // escape
 					$scope.cancelEdit();
-					break
-				case 13: // return
-					$scope.saveEdit();
-					break
+					break;
 			}
 		} else {
-			if (cix == -1) {
-				switch (e.keyCode) {
-					case 38: // up
-					case 75: // k
-						if (rix > 0) {
-							deselectRow();
-							rix--;
-							selectRow();
-						}
-						break;
-					case 40: // down
-					case 74: // j
-						if (rix < $scope.rows.length-1) {
-							deselectRow();
-							rix++;
-							selectRow();
-						}
-						break;
-					case 32: // space
-						cix = 0;
-						selectColumn();
-						break;
-				}
-			} else {
-				switch (e.keyCode) {
-					case 37: // left
-					case 72: // h
-						if (cix > 0) {
-							deselectColumn();
-							cix--;
-							selectColumn();
-						}
-						break;
-					case 39: // right
-					case 76: // l
-						if (cix < columns.length-1) {
-							deselectColumn();
-							cix++;
-							selectColumn();
-						}
+			switch (e.keyCode) {
+				case 37: // left
+				case 72: // h
+					goLeft();
 					break;
-					case 38: // up
-					case 75: // k
-						if (iix > 0) {
-							deselectItem();
-							iix--;
-							selectItem();
-						}
-						break;
-					case 40: // down
-					case 74: // j
-						if (iix < $scope.rows[rix][$scope.selectedColumnName].length-1) {
-							deselectItem();
-							iix++;
-							selectItem();
-						}
-						break;
-					case 13: // return
-						doEdit();
-						break;
-					case 32: // space
-						deselectColumn();
-						cix = -1;
-						break;
-				}
+				case 39: // right
+				case 76: // l
+					goRight();
+					break;
+				case 38: // up
+				case 75: // k
+					goUp();
+					break;
+				case 40: // down
+				case 74: // j
+					goDown();
+					break;
+				case 13: // enter
+					startEdit();
+					break;
+			}
+		}
+	}
+
+	function goLeft() {
+		if ($scope.cix > 0) {
+			$scope.cix--;
+			$scope.iix = 0;
+		}
+	}
+
+	function goRight() {
+		if ($scope.cix < $scope.columns.length - 1) {
+			$scope.cix++;
+			$scope.iix = 0;
+		}
+	}
+
+	function goUp() {
+		if ($scope.iix > 0) {
+			$scope.iix--;
+		} else {
+			if ($scope.rix > 0) {
+				$scope.rix--;
+				$scope.iix = getNumItems($scope.rix, $scope.cix) - 1;
+			}
+		}
+	}
+
+	function goDown() {
+		if ($scope.iix < getNumItems($scope.rix, $scope.cix) - 1) {
+			$scope.iix++;
+		} else {
+			if ($scope.rix < $scope.rows.length - 1) {
+				$scope.rix++;
+				$scope.iix = 0;
 			}
 		}
 	}
@@ -221,7 +209,9 @@ app.directive('field', function() {
 				iterable = '[' + iterable + ']';
 			}
 
-			return '<td><ul><li ng-repeat="item in ' + iterable + '"><fielditem column="' + column + '"/></ul></td>';
+			// $parent.$index is the row index
+			// $index is the item index
+			return '<ul><li ng-repeat="item in ' + iterable + '" ng-class="{selected: $parent.$index == rix && getCix(\'' + column + '\') == cix && $index == iix}"><fielditem column="' + column + '"/></li></ul>';
 		},
 	}
 });
@@ -234,3 +224,5 @@ app.directive('fielditem', function() {
 		},
 	}
 });
+
+
