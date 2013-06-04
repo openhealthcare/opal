665e34c5fdf854f3e1b94aa9e998881cf1ef781d
Peter Inglesby <peter.inglesby@gmail.com>
2013-06-04 12:24:59 +0100

    Serve static files with staticfiles

--- opal/urls.py
+++ opal/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,3 +16,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
