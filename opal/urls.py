03369f866fd6f5c2255dc593dd3c496bdcf5ea71
Peter Inglesby <peter.inglesby@gmail.com>
2013-06-10 10:48:31 +0100

    Add feedback form
    Addresses #24.

--- opal/urls.py
+++ opal/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
-from patients.views import IndexView
+from patients.views import IndexView, ContactView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view()),
+    url(r'^contact/$', ContactView.as_view()),
     url(r'^options/', include('options.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- patients/views.py
+++ patients/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-        template_name = "opal.html"
+    template_name = "opal.html"
+
+# This probably doesn't belong here
+class ContactView(TemplateView):
+    template_name = "contact.html"
