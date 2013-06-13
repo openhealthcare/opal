6ead5b83ea18c7ce1ad08a70d590d19853617c21
Peter Inglesby <peter.inglesby@gmail.com>
2013-06-13 02:19:18 +0100

    Add comment

--- patients/serializers.py
+++ patients/serializers.py
@@ -7,12 +7,20 @@ class PatientSerializer(serializers.ModelSerializer):
         model = models.Patient
 
 def build_subrecord_serializer(model):
+    '''Builds a serializer for a Subrecord, handling ForeignKeyOrFreeText correctly.
+
+    eg build_subrecord_serializer(Diagnosis) returns a serialzer that could've been defined:
+
+    class DiagnosisSerializer(serializers.ModelSerializer):
+        condition = serializers.WritableField(required=False)
+        class Meta:
+            model = Diagnosis
+            exclude = ['condition']
+    '''
     fkft_field_names = [name for name, value in vars(model).items() if isinstance(value, ForeignKeyOrFreeText)]
     field_names_to_exclude = []
     for name in fkft_field_names:
         field_names_to_exclude.extend([name + '_fk', name + '_ft'])
     attrs = {name: serializers.WritableField(required=False) for name in fkft_field_names}
     attrs['Meta'] = type('Meta', (object,), {'model': model, 'exclude': field_names_to_exclude})
-    return type('name', (serializers.ModelSerializer,), attrs)
-
-
+    return type(model.__name__ + 'Serializer', (serializers.ModelSerializer,), attrs)
