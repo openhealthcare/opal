7b45b29e308f9274b018019571f83feb6c9cea04
Peter Inglesby <peter.inglesby@gmail.com>
2013-07-26 13:32:49 +0100

    Remove unused code

--- options/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from options import model_names
-
-urlpatterns = patterns('')
-
-for name in model_names:
-    urlpatterns += patterns('', url(r'^%s_list/' % name, 'options.views.options_view', {'model_name': name}))
