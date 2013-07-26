c13694fbb4f2c9ddbf99dbd2e388a48b480bc605
David Miller <david@deadpansincerity.com>
2013-07-26 17:45:36 +0100

    Create reversion history for changes to the Patients app.
    Use Django-Reversion[1], which adds a custom admin subclass that includes
    the ability to see all previous versions of an object and revert to them.
    
    [1] https://github.com/etianen/django-reversion

--- opal/settings.py
+++ opal/settings.py
@@ -97,6 +97,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'reversion.middleware.RevisionMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -132,6 +133,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'reversion',
     'south',
     'options',
     'patients',
