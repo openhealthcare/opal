0f62b0ca4d8cdf5af20ebb7526bba90be7eabad9
Peter Inglesby <peter.inglesby@gmail.com>
2013-07-29 16:09:28 +0100

    Reinstate RevisionMiddleware

--- opal/settings.py
+++ opal/settings.py
@@ -113,7 +113,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-#    'reversion.middleware.RevisionMiddleware',
+    'reversion.middleware.RevisionMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
