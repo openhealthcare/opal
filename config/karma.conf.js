d6eee1f45983c3d5b0267de4a448fc7ffaa6da91
Peter Inglesby <peter.inglesby@gmail.com>
2013-08-05 09:00:04 +0100

    Add infrastructure for karma tests

---
+++ config/karma.conf.js
@@ -0,0 +1,14 @@
+basePath = '../opal/assets/js';
+
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'angular*.js',
+  'underscore.js',
+  'opal/services.js',
+  'opaltest/servicesTest.js'
+];
+
+autoWatch = true;
+
+browsers = ['Chrome'];
