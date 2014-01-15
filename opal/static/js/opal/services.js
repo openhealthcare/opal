49e2b9ac26efdf01b4715fafa690d933009b5fe3
David Miller <david@deadpansincerity.com>
2014-01-15 13:56:05 +0000

    Minor js formatting.

--- opal/static/js/opal/services.js
+++ opal/static/js/opal/services.js
@@ -1,5 +1,7 @@
 // TODO make this a service
-var CATEGORIES = ['Inepisode', 'Review', 'Followup', 'Transferred', 'Discharged', 'Deceased'];
+var CATEGORIES = [
+    'Inepisode', 'Review', 'Followup', 'Transferred', 'Discharged', 'Deceased'
+];
 
 var services = angular.module('opal.services', ['ngResource']);
 
