ddfdc91e6500addaac358198b24c1a779e5e4a91
David Miller <david@deadpansincerity.com>
2014-05-21 17:01:08 +0100

    Improve unicode handling for data dumps.
    closes openhealthcare/elcid#234

--- opal/utils/__init__.py
+++ opal/utils/__init__.py
@@ -108,18 +108,18 @@ def json_to_csv(episodes, description, user):
             tags = ";".join(current)
 
             episode_csv.append([
-                    str(e['id']),
-                    str(e['date_of_admission']),
-                    str(e['discharge_date']),
-                    str(e['demographics'][0]['hospital_number']),
-                    str(e['demographics'][0]['date_of_birth']),
-                    str(e['demographics'][0]['country_of_birth']),
-                    str(e['demographics'][0]['ethnicity']),
-                    str(e['location'][0]['category']),
+                    unicode(e['id']),
+                    unicode(e['date_of_admission']),
+                    unicode(e['discharge_date']),
+                    unicode(e['demographics'][0]['hospital_number']),
+                    unicode(e['demographics'][0]['date_of_birth']),
+                    unicode(e['demographics'][0]['country_of_birth']),
+                    unicode(e['demographics'][0]['ethnicity']),
+                    unicode(e['location'][0]['category']),
                     tags,
-                    str(e['location'][0]['hospital']),
-                    str(e['location'][0]['ward']),
-                    str(e['location'][0]['bed']),
+                    unicode(e['location'][0]['hospital']),
+                    unicode(e['location'][0]['ward']),
+                    unicode(e['location'][0]['bed']),
                     ])
             for diagnosis in e['diagnosis']:
                 coded = ''
@@ -127,23 +127,23 @@ def json_to_csv(episodes, description, user):
                     coded = diagnosis['condition']
 
                 diagnosis_csv.append([
-                        str(e['id']),
+                        unicode(e['id']),
                         coded,
-                        str(diagnosis['condition_ft']),
-                        str(diagnosis['provisional']),
-                        str(diagnosis['details']),
-                        str(diagnosis['date_of_diagnosis'])
+                        unicode(diagnosis['condition_ft']),
+                        unicode(diagnosis['provisional']),
+                        unicode(diagnosis['details']),
+                        unicode(diagnosis['date_of_diagnosis'])
                         ])
             for pmh in e['past_medical_history']:
                 coded = ''
                 if pmh['condition_fk_id']:
                     coded = pmh['condition']
                 pmh_csv.append([
-                        str(e['id']),
+                        unicode(e['id']),
                         coded,
-                        str(pmh['condition_ft']),
-                        str(pmh['year']),
-                        str(pmh['details'])
+                        unicode(pmh['condition_ft']),
+                        unicode(pmh['year']),
+                        unicode(pmh['details'])
                         ])
             for antimicrobial in e['antimicrobial']:
                 coded = ''
@@ -151,20 +151,20 @@ def json_to_csv(episodes, description, user):
                     coded = antimicrobial['drug']
 
                 antimicrobials_csv.append([
-                        str(e['id']),
+                        unicode(e['id']),
                         coded,
-                        str(antimicrobial['drug_ft']),
-                        str(antimicrobial['dose']),
-                        str(antimicrobial['route']),
-                        str(antimicrobial['start_date']),
-                        str(antimicrobial['end_date'])
+                        unicode(antimicrobial['drug_ft']),
+                        unicode(antimicrobial['dose']),
+                        unicode(antimicrobial['route']),
+                        unicode(antimicrobial['start_date']),
+                        unicode(antimicrobial['end_date'])
                         ])
             for allergy in e['allergies']:
                 allergies_csv.append([
-                        str(e['id']),
-                        str(allergy['drug']),
-                        str(allergy['provisional']),
-                        str(allergy['details'])
+                        unicode(e['id']),
+                        unicode(allergy['drug']),
+                        unicode(allergy['provisional']),
+                        unicode(allergy['details'])
                         ])
             for travel in e['travel']:
                 coded_dest, coded_reason = '', ''
@@ -174,76 +174,76 @@ def json_to_csv(episodes, description, user):
                     coded_reason = travel['reason_for_travel']
 
                 travel_csv.append([
-                        str(e['id']),
+                        unicode(e['id']),
                         coded_dest,
-                        str(travel['destination_ft']),
-                        str(travel['dates']),
+                        unicode(travel['destination_ft']),
+                        unicode(travel['dates']),
                         coded_reason,
-                        str(travel['reason_for_travel_ft']),
-                        str(travel['specific_exposures'])
+                        unicode(travel['reason_for_travel_ft']),
+                        unicode(travel['specific_exposures'])
                         ])
 
             for advice in e['microbiology_input']:
                 clinical_advice_csv.append([
-                        str(e['id']),
-                        str(advice['date']),
-                        str(advice['initials']),
-                        str(advice['reason_for_interaction']),
-                        str(advice['clinical_discussion']),
-                        str(advice['agreed_plan']),
-                        str(advice['discussed_with']),
-                        str(advice['clinical_advice_given']),
-                        str(advice['infection_control_advice_given']),
-                        str(advice['change_in_antibiotic_prescription']),
-                        str(advice['referred_to_opat']),
+                        unicode(e['id']),
+                        unicode(advice['date']),
+                        unicode(advice['initials']),
+                        unicode(advice['reason_for_interaction']),
+                        unicode(advice['clinical_discussion']),
+                        unicode(advice['agreed_plan']),
+                        unicode(advice['discussed_with']),
+                        unicode(advice['clinical_advice_given']),
+                        unicode(advice['infection_control_advice_given']),
+                        unicode(advice['change_in_antibiotic_prescription']),
+                        unicode(advice['referred_to_opat']),
                         ])
             for test in e['microbiology_test']:
 
                 investigations_csv.append([
-                        str(e['id']),
-                        str(test['test']),
-                        str(test['date_ordered']),
-                        str(test['details']),
-                        str(test['microscopy']),
-                        str(test['organism']),
-                        str(test['sensitive_antibiotics']),
-                        str(test['resistant_antibiotics']),
-                        str(test['result']),
-                        str(test['igm']),
-                        str(test['igg']),
-                        str(test['vca_igm']),
-                        str(test['vca_igg']),
-                        str(test['ebna_igg']),
-                        str(test['hbsag']),
-                        str(test['anti_hbs']),
-                        str(test['anti_hbcore_igm']),
-                        str(test['anti_hbcore_igg']),
-                        str(test['rpr']),
-                        str(test['tppa']),
-                        str(test['viral_load']),
-                        str(test['parasitaemia']),
-                        str(test['hsv']),
-                        str(test['vzv']),
-                        str(test['syphilis']),
-                        str(test['c_difficile_antigen']),
-                        str(test['c_difficile_toxin']),
-                        str(test['species']),
-                        str(test['hsv_1']),
-                        str(test['hsv_2']),
-                        str(test['enterovirus']),
-                        str(test['cmv']),
-                        str(test['ebv']),
-                        str(test['influenza_a']),
-                        str(test['influenza_b']),
-                        str(test['parainfluenza']),
-                        str(test['metapneumovirus']),
-                        str(test['rsv']),
-                        str(test['adenovirus']),
-                        str(test['norovirus']),
-                        str(test['rotavirus']),
-                        str(test['giardia']),
-                        str(test['entamoeba_histolytica']),
-                        str(test['cryptosporidium'])
+                        unicode(e['id']),
+                        unicode(test['test']),
+                        unicode(test['date_ordered']),
+                        unicode(test['details']),
+                        unicode(test['microscopy']),
+                        unicode(test['organism']),
+                        unicode(test['sensitive_antibiotics']),
+                        unicode(test['resistant_antibiotics']),
+                        unicode(test['result']),
+                        unicode(test['igm']),
+                        unicode(test['igg']),
+                        unicode(test['vca_igm']),
+                        unicode(test['vca_igg']),
+                        unicode(test['ebna_igg']),
+                        unicode(test['hbsag']),
+                        unicode(test['anti_hbs']),
+                        unicode(test['anti_hbcore_igm']),
+                        unicode(test['anti_hbcore_igg']),
+                        unicode(test['rpr']),
+                        unicode(test['tppa']),
+                        unicode(test['viral_load']),
+                        unicode(test['parasitaemia']),
+                        unicode(test['hsv']),
+                        unicode(test['vzv']),
+                        unicode(test['syphilis']),
+                        unicode(test['c_difficile_antigen']),
+                        unicode(test['c_difficile_toxin']),
+                        unicode(test['species']),
+                        unicode(test['hsv_1']),
+                        unicode(test['hsv_2']),
+                        unicode(test['enterovirus']),
+                        unicode(test['cmv']),
+                        unicode(test['ebv']),
+                        unicode(test['influenza_a']),
+                        unicode(test['influenza_b']),
+                        unicode(test['parainfluenza']),
+                        unicode(test['metapneumovirus']),
+                        unicode(test['rsv']),
+                        unicode(test['adenovirus']),
+                        unicode(test['norovirus']),
+                        unicode(test['rotavirus']),
+                        unicode(test['giardia']),
+                        unicode(test['entamoeba_histolytica']),
+                        unicode(test['cryptosporidium'])
                         ])
         except UnicodeDecodeError:
             continue
@@ -261,14 +261,14 @@ def json_to_csv(episodes, description, user):
         zipfolder = '{0}.{1}/'.format(user.username, datetime.date.today())
         with tempdir:
             zipfile = archive.ZipPath('episodes.zip')
-            zipfile/(zipfolder+'episodes.csv') << episode_csv
-            zipfile/(zipfolder+'diagnosis.csv') << diagnosis_csv
-            zipfile/(zipfolder+'past_medical_history.csv') << pmh_csv
-            zipfile/(zipfolder+'antimicrobials.csv') << antimicrobials_csv
-            zipfile/(zipfolder+'allergies.csv') << allergies_csv
-            zipfile/(zipfolder+'travel.csv') << travel_csv
+            zipfile/(zipfolder+'episodes.csv') << episode_csv.encode('UTF-8')
+            zipfile/(zipfolder+'diagnosis.csv') << diagnosis_csv.encode('UTF-8')
+            zipfile/(zipfolder+'past_medical_history.csv') << pmh_csv.encode('UTF-8')
+            zipfile/(zipfolder+'antimicrobials.csv') << antimicrobials_csv.encode('UTF-8')
+            zipfile/(zipfolder+'allergies.csv') << allergies_csv.encode('UTF-8')
+            zipfile/(zipfolder+'travel.csv') << travel_csv.encode('UTF-8')
             zipfile/(zipfolder+'clinical_advice.csv') << clinical_advice_csv.encode('UTF-8')
-            zipfile/(zipfolder+'investigations.csv') << investigations_csv
-            zipfile/(zipfolder+'filter.txt') << description
+            zipfile/(zipfolder+'investigations.csv') << investigations_csv.encode('UTF-8')
+            zipfile/(zipfolder+'filter.txt') << description.encode('UTF-8')
             ffs.mv(zipfile, target)
     return target
