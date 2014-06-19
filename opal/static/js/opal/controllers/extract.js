52e5f5d38fe18b488f25e7d5c95390d20f4a6104
David Miller <david@deadpansincerity.com>
2014-06-19 15:26:55 +0100

    Use lookup lists in extract interface.
    refs openhealthcare/elcid#133 openhealthcare/elcid#134

--- opal/static/js/opal/controllers/extract.js
+++ opal/static/js/opal/controllers/extract.js
@@ -2,128 +2,159 @@ angular.module('opal.controllers').controller(
     'ExtractCtrl', function($scope, $http, $window, $modal,
                             filters, options, schema){
 
-    $scope.JSON = window.JSON;
-    $scope.state =  'normal';
-    $scope.filters = filters;
-    $scope.columns = schema.columns;
-    $scope.column_names = _.map(schema.columns, function(c){
-        return c.name.underscoreToCapWords();
-    });
+        $scope.JSON = window.JSON;
+        $scope.state =  'normal';
+        $scope.filters = filters;
+        $scope.columns = schema.columns;
+        $scope.column_names = _.map(schema.columns, function(c){
+            return c.name.underscoreToCapWords();
+        });
 
-	for (var name in options) {
-		$scope[name + '_list'] = options[name];
-	};
+	    for (var name in options) {
+		    $scope[name + '_list'] = options[name];
+	    };
 
-    $scope.model = {
-        combine  : "and",
-        column   : null,
-        field    : null,
-        queryType: "Equals",
-        query    : null
-    };
+        $scope.model = {
+            combine    : "and",
+            column     : null,
+            field      : null,
+            queryType  : "Equals",
+            query      : null,
+            lookup_list: []
+        };
 
-    $scope.criteria = [_.clone($scope.model)];
+        $scope.criteria = [_.clone($scope.model)];
 
-    $scope.completeCriteria =  function(){
-        return _.filter($scope.criteria, function(c){
-            if(c.column &&  c.field &&  c.query){
-                return true
+        $scope.completeCriteria =  function(){
+            return _.filter($scope.criteria, function(c){
+                if(c.column &&  c.field &&  c.query){
+                    return true
+                }
+                return false
+            })
+        };
+
+        $scope.searchableFields = function(column){
+            // TODO - don't hard-code this
+            if(column.name == 'microbiology_test'){
+                return [
+                    'Test',
+                    'Date Ordered',
+                    'Details',
+                    'Microscopy',
+                    'Organism',
+                    'Sensitive Antibiotics',
+                    'Resistant Antibiotics'
+                ]
             }
-            return false
-        })
-    };
+            return _.map(
+                _.reject(
+                    column.fields,
+                    function(c){ return c.type == 'token' ||  c.type ==  'list'; }),
+                function(c){ return c.name.underscoreToCapWords(); }
+            );
+        };
 
-    $scope.searchableFields = function(column){
-        // TODO - don't hard-code this
-        if(column.name == 'microbiology_test'){
-            return [
-                'Test',
-                'Date Ordered',
-                'Details',
-                'Microscopy',
-                'Organism',
-                'Sensitive Antibiotics',
-                'Resistant Antibiotics'
-            ]
+        $scope.isType = function(column, field, type){
+            if(!column || !field){
+                return false;
+            }
+            var col = _.find($scope.columns, function(item){return item.name == column.toLowerCase().replace( / /g,  '_')});
+            var theField =  _.find(col.fields, function(f){return f.name == field.toLowerCase().replace( / /g,  '_')});
+            if(!theField){ return false }
+            return theField.type == type;
+        };
+
+        $scope.isBoolean = function(column, field){
+            return $scope.isType(column, field, "boolean");
+        };
+
+        $scope.isText = function(column, field){
+            return $scope.isType(column, field, "string") || $scope.isType(column, field, "text");
         }
-        return _.map(
-            _.reject(
-                column.fields,
-                function(c){ return c.type == 'token' ||  c.type ==  'list'; }),
-            function(c){ return c.name.underscoreToCapWords(); }
-        );
-    };
 
-    $scope.isType = function(column, field, type){
-        if(!column || !field){
-            return false;
-        }
-        var col = _.find($scope.columns, function(item){return item.name == column.toLowerCase().replace( / /g,  '_')});
-        var theField =  _.find(col.fields, function(f){return f.name == field.toLowerCase().replace( / /g,  '_')});
-        return theField.type == type;
-    };
+        $scope.isDate = function(column, field){
+            return $scope.isType(column, field, "date");
+        };
 
-    $scope.isBoolean = function(column, field){
-        return $scope.isType(column, field, "boolean");
-    };
+        $scope.addFilter = function(){
+            $scope.criteria.push(_.clone($scope.model));
+        };
 
-    $scope.isText = function(column, field){
-        return $scope.isType(column, field, "string") || $scope.isType(column, field, "text");
-    }
+        $scope.removeFilter = function(index){
+            $scope.criteria.splice(index, 1);
+        };
 
-    $scope.isDate = function(column, field){
-        return $scope.isType(column, field, "date");
-    };
-
-    $scope.addFilter = function(){
-        $scope.criteria.push(_.clone($scope.model));
-    };
-
-    $scope.removeFilter = function(index){
-        $scope.criteria.splice(index, 1);
-    };
-
-    $scope.search = function(){
-        $scope.state = 'pending';
-        $http.post('/search/extract/', $scope.completeCriteria()).success(
-            function(results){
-                $scope.results = results;
-                $scope.state = 'normal';
-            }).error(function(){
-                $scope.state = 'normal';
-                $window.alert('ERROR: Could not process this search. Please report it to the OPAL team')
+        //
+        // Determine the appropriate lookup list for this field if
+        // one exists.
+        //
+       $scope.$watch('criteria', function(){
+            _.map($scope.criteria, function(c){
+                var column = _.findWhere($scope.columns, {name: c.column});
+                if(!column){return}
+                if(!c.field){return}
+                var field = _.findWhere(column.fields, {name: c.field.toLowerCase().replace(/ /g, '_')});
+                if(!field){return}
+                if(field.lookup_list){
+                    c.lookup_list = $scope[field.lookup_list + '_list'];
+                }
             });
-    };
+        }, true);
 
-    $scope.jumpToFilter = function($event, filter){
-        $event.preventDefault()
-        $scope.criteria = filter.criteria;
-    }
+        // $scope.geLookupValues = function(query){
+        //     var column = _.findWhere($scope.columns, {name: c.column});
+        //     if(!column){return []}
+        //     if(!c.field){return []}
+        //     var field = _.findWhere(column.fields, {name: c.field.toLowerCase()});
+        //     if(field.lookup_list){
+        //         return $scope[field.lookup_list + '_list'];
+        //     }else{
+        //         return []
+        //     }
+        // }
 
-    $scope.editFilter = function($event, filter, $index){
-        $event.preventDefault();
-		modal = $modal.open({
-			templateUrl: '/templates/modals/save_filter_modal.html/',
-			controller: 'SaveFilterCtrl',
-			resolve: {
-				params: function() { return $scope.filters[$index]; },
-			}
-		}).result.then(function(result){
-            $scope.filters[$index] = result;
-        });
-    }
+        $scope.search = function(){
+            $scope.state = 'pending';
+            $http.post('/search/extract/', $scope.completeCriteria()).success(
+                function(results){
+                    $scope.results = results;
+                    $scope.state = 'normal';
+                }).error(function(){
+                    $scope.state = 'normal';
+                    $window.alert('ERROR: Could not process this search. Please report it to the OPAL team')
+                });
+        };
 
-    $scope.save = function(){
+        $scope.jumpToFilter = function($event, filter){
+            $event.preventDefault()
+            $scope.criteria = filter.criteria;
+        }
 
-		modal = $modal.open({
-			templateUrl: '/templates/modals/save_filter_modal.html/',
-			controller: 'SaveFilterCtrl',
-			resolve: {
-				params: function() { return {name: null, criteria: $scope.criteria}; },
-			}
-		}).result.then(function(result){
-            $scope.filters.push(result);
-        });
-    };
+        $scope.editFilter = function($event, filter, $index){
+            $event.preventDefault();
+		    modal = $modal.open({
+			    templateUrl: '/templates/modals/save_filter_modal.html/',
+			    controller: 'SaveFilterCtrl',
+			    resolve: {
+				    params: function() { return $scope.filters[$index]; },
+			    }
+		    }).result.then(function(result){
+                $scope.filters[$index] = result;
+            });
+        }
 
-});
+        $scope.save = function(){
+
+		    modal = $modal.open({
+			    templateUrl: '/templates/modals/save_filter_modal.html/',
+			    controller: 'SaveFilterCtrl',
+			    resolve: {
+				    params: function() { return {name: null, criteria: $scope.criteria}; },
+			    }
+		    }).result.then(function(result){
+                $scope.filters.push(result);
+            });
+        };
+
+    });
