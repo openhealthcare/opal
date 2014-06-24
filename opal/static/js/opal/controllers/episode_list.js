04c52fd632dbb6125aabf26b6f8ff163f9d9600b
David Miller <david@deadpansincerity.com>
2014-06-24 12:48:58 +0100

    Hide tooltip by default for now.
    refs openhealthcare/elcid#240

--- opal/static/js/opal/controllers/episode_list.js
+++ opal/static/js/opal/controllers/episode_list.js
@@ -7,13 +7,13 @@ angular.module('opal.controllers').controller(
                                 episodeVisibility, viewDischarged){
 
         var version = window.version;
-        if($cookieStore.get('opal.seenVersion') !=  version){
-            $('#changelogTooltip').tooltip('show');
-            $('#changelog').on('hidden.bs.modal', function(){
-                $cookieStore.put('opal.seenVersion', version)
-                $('#changelogTooltip').tooltip('hide');
-            });
-        }
+        // if($cookieStore.get('opal.seenVersion') !=  version){
+        //     $('#changelogTooltip').tooltip('show');
+        //     $('#changelog').on('hidden.bs.modal', function(){
+        //         $cookieStore.put('opal.seenVersion', version)
+        //         $('#changelogTooltip').tooltip('hide');
+        //     });
+        // }
 
 
         $scope.state = 'normal';
