dcad6e7ee7343d026829196ea7a599521e7db3be
David Miller <david@deadpansincerity.com>
2014-07-30 19:04:18 +0200

    Update plugin refs openhealthcare/elcid#241

--- opal/static/js/jquery-plugins/jquery.stickytableheaders.js
+++ opal/static/js/jquery-plugins/jquery.stickytableheaders.js
@@ -1,12 +1,15 @@
 /*! Copyright (c) 2011 by Jonas Mosbech - https://github.com/jmosbech/StickyTableHeaders
-    MIT license info: https://github.com/jmosbech/StickyTableHeaders/blob/master/license.txt */
+	MIT license info: https://github.com/jmosbech/StickyTableHeaders/blob/master/license.txt */
 
 ;(function ($, window, undefined) {
 	'use strict';
 
-	var name = 'stickyTableHeaders';
-	var defaults = {
-			fixedOffset: 0
+	var name = 'stickyTableHeaders',
+		id = 0,
+		defaults = {
+			fixedOffset: 0,
+			leftOffset: 0,
+			scrollableArea: window
 		};
 
 	function Plugin (el, options) {
@@ -17,24 +20,25 @@
 		// Access to jQuery and DOM versions of element
 		base.$el = $(el);
 		base.el = el;
+		base.id = id++;
+		base.$window = $(window);
+		base.$document = $(document);
 
 		// Listen for destroyed, call teardown
 		base.$el.bind('destroyed',
 			$.proxy(base.teardown, base));
 
 		// Cache DOM refs for performance reasons
-		base.$window = $(window);
 		base.$clonedHeader = null;
 		base.$originalHeader = null;
 
 		// Keep track of state
 		base.isSticky = false;
+		base.hasBeenSticky = false;
 		base.leftOffset = null;
 		base.topOffset = null;
 
 		base.init = function () {
-			base.options = $.extend({}, defaults, options);
-
 			base.$el.each(function () {
 				var $this = $(this);
 
@@ -43,6 +47,7 @@
 
 				base.$originalHeader = $('thead:first', this);
 				base.$clonedHeader = base.$originalHeader.clone();
+				$this.trigger('clonedHeader.' + name, [base.$clonedHeader]);
 
 				base.$clonedHeader.addClass('tableFloatingHeader');
 				base.$clonedHeader.css('display', 'none');
@@ -58,9 +63,9 @@
 				$('head').append(base.$printStyle);
 			});
 
+			base.setOptions(options);
 			base.updateWidth();
 			base.toggleHeaders();
-
 			base.bind();
 		};
 
@@ -70,6 +75,9 @@
 		};
 
 		base.teardown = function(){
+			if (base.isSticky) {
+				base.$originalHeader.css('position', 'static');
+			}
 			$.removeData(base.el, 'plugin_' + name);
 			base.unbind();
 
@@ -83,56 +91,87 @@
 		};
 
 		base.bind = function(){
-			base.$window.on('scroll.' + name, base.toggleHeaders);
-			base.$window.on('resize.' + name, base.toggleHeaders);
-			base.$window.on('resize.' + name, base.updateWidth);
+			base.$scrollableArea.on('scroll.' + name, base.toggleHeaders);
+			if (!base.isWindowScrolling) {
+				base.$window.on('scroll.' + name + base.id, base.setPositionValues);
+				base.$window.on('resize.' + name + base.id, base.toggleHeaders);
+			}
+			base.$scrollableArea.on('resize.' + name, base.toggleHeaders);
+			base.$scrollableArea.on('resize.' + name, base.updateWidth);
 		};
 
 		base.unbind = function(){
 			// unbind window events by specifying handle so we don't remove too much
-			base.$window.off('.' + name, base.toggleHeaders);
-			base.$window.off('.' + name, base.updateWidth);
+			base.$scrollableArea.off('.' + name, base.toggleHeaders);
+			if (!base.isWindowScrolling) {
+				base.$window.off('.' + name + base.id, base.setPositionValues);
+				base.$window.off('.' + name + base.id, base.toggleHeaders);
+			}
+			base.$scrollableArea.off('.' + name, base.updateWidth);
 			base.$el.off('.' + name);
 			base.$el.find('*').off('.' + name);
 		};
 
 		base.toggleHeaders = function () {
-			base.$el.each(function () {
-				var $this = $(this);
+			if (base.$el) {
+				base.$el.each(function () {
+					var $this = $(this),
+						newLeft,
+						newTopOffset = base.isWindowScrolling ? (
+									isNaN(base.options.fixedOffset) ?
+									base.options.fixedOffset.outerHeight() :
+									base.options.fixedOffset
+								) :
+								base.$scrollableArea.offset().top + (!isNaN(base.options.fixedOffset) ? base.options.fixedOffset : 0),
+						offset = $this.offset(),
 
-				var newTopOffset = isNaN(base.options.fixedOffset) ?
-					base.options.fixedOffset.height() : base.options.fixedOffset;
+						scrollTop = base.$scrollableArea.scrollTop() + newTopOffset,
+						scrollLeft = base.$scrollableArea.scrollLeft(),
 
-				var offset = $this.offset();
-				var scrollTop = base.$window.scrollTop() + newTopOffset;
-				var scrollLeft = base.$window.scrollLeft();
+						scrolledPastTop = base.isWindowScrolling ?
+								scrollTop > offset.top :
+								newTopOffset > offset.top,
+						notScrolledPastBottom = (base.isWindowScrolling ? scrollTop : 0) <
+								(offset.top + $this.height() - base.$clonedHeader.height() - (base.isWindowScrolling ? 0 : newTopOffset));
 
-				if ((scrollTop > offset.top) && (scrollTop < offset.top + $this.height() - base.$clonedHeader.height())) {
-					var newLeft = offset.left - scrollLeft;
-					if (base.isSticky && (newLeft === base.leftOffset) && (newTopOffset === base.topOffset)) {
-						return;
+					if (scrolledPastTop && notScrolledPastBottom) {
+						newLeft = offset.left - scrollLeft + base.options.leftOffset;
+						base.$originalHeader.css({
+							'position': 'fixed',
+							'margin-top': 0,
+							'left': newLeft,
+							'z-index': 1 // #18: opacity bug
+						});
+						base.leftOffset = newLeft;
+						base.topOffset = newTopOffset;
+						base.$clonedHeader.css('display', '');
+						if (!base.isSticky) {
+							base.isSticky = true;
+							// make sure the width is correct: the user might have resized the browser while in static mode
+							base.updateWidth();
+						}
+						base.setPositionValues();
+					} else if (base.isSticky) {
+						base.$originalHeader.css('position', 'static');
+						base.$clonedHeader.css('display', 'none');
+						base.isSticky = false;
+						base.resetWidth($('td,th', base.$clonedHeader), $('td,th', base.$originalHeader));
 					}
+				});
+			}
+		};
 
-					base.$originalHeader.css({
-						'position': 'fixed',
-						'top': newTopOffset,
-						'margin-top': 0,
-						'left': newLeft,
-						'z-index': 1 // #18: opacity bug
-					});
-					base.$clonedHeader.css('display', '');
-					base.isSticky = true;
-					base.leftOffset = newLeft;
-					base.topOffset = newTopOffset;
-
-					// make sure the width is correct: the user might have resized the browser while in static mode
-					base.updateWidth();
-				}
-				else if (base.isSticky) {
-					base.$originalHeader.css('position', 'static');
-					base.$clonedHeader.css('display', 'none');
-					base.isSticky = false;
-				}
+		base.setPositionValues = function () {
+			var winScrollTop = base.$window.scrollTop(),
+				winScrollLeft = base.$window.scrollLeft();
+			if (!base.isSticky ||
+					winScrollTop < 0 || winScrollTop + base.$window.height() > base.$document.height() ||
+					winScrollLeft < 0 || winScrollLeft + base.$window.width() > base.$document.width()) {
+				return;
+			}
+			base.$originalHeader.css({
+				'top': base.topOffset - (base.isWindowScrolling ? 0 : winScrollTop),
+				'left': base.leftOffset - (base.isWindowScrolling ? 0 : winScrollLeft)
 			});
 		};
 
@@ -141,21 +180,80 @@
 				return;
 			}
 			// Copy cell widths from clone
-			var $origHeaders = $('th,td', base.$originalHeader);
-			$('th,td', base.$clonedHeader).each(function (index) {
-				var width = $(this).width();
-				$origHeaders.eq(index).css({
-					'min-width': width,
-					'max-width': width
-				});
-			});
+			if (!base.$originalHeaderCells) {
+				base.$originalHeaderCells = $('th,td', base.$originalHeader);
+			}
+			if (!base.$clonedHeaderCells) {
+				base.$clonedHeaderCells = $('th,td', base.$clonedHeader);
+			}
+			var cellWidths = base.getWidth(base.$clonedHeaderCells);
+			base.setWidth(cellWidths, base.$clonedHeaderCells, base.$originalHeaderCells);
 
 			// Copy row width from whole table
 			base.$originalHeader.css('width', base.$clonedHeader.width());
 		};
 
-		base.updateOptions = function(options) {
+		base.getWidth = function ($clonedHeaders) {
+			var widths = [];
+			$clonedHeaders.each(function (index) {
+				var width, $this = $(this);
+
+				if ($this.css('box-sizing') === 'border-box') {
+					width = $this.outerWidth(); // #39: border-box bug
+				} else {
+					var $origTh = $('th', base.$originalHeader);
+					if ($origTh.css('border-collapse') === 'collapse') {
+						if (window.getComputedStyle) {
+							width = parseFloat(window.getComputedStyle(this, null).width);
+						} else {
+							// ie8 only
+							var leftPadding = parseFloat($this.css('padding-left'));
+							var rightPadding = parseFloat($this.css('padding-right'));
+							// Needs more investigation - this is assuming constant border around this cell and it's neighbours.
+							var border = parseFloat($this.css('border-width'));
+							width = $this.outerWidth() - leftPadding - rightPadding - border;
+						}
+					} else {
+						width = $this.width();
+					}
+				}
+
+				widths[index] = width;
+			});
+			return widths;
+		};
+
+		base.setWidth = function (widths, $clonedHeaders, $origHeaders) {
+			$clonedHeaders.each(function (index) {
+				var width = widths[index];
+				$origHeaders.eq(index).css({
+					'min-width': width,
+					'max-width': width
+				});
+			});
+		};
+
+		base.resetWidth = function ($clonedHeaders, $origHeaders) {
+			$clonedHeaders.each(function (index) {
+				var $this = $(this);
+				$origHeaders.eq(index).css({
+					'min-width': $this.css('min-width'),
+					'max-width': $this.css('max-width')
+				});
+			});
+		};
+
+		base.setOptions = function (options) {
 			base.options = $.extend({}, defaults, options);
+			base.$scrollableArea = $(base.options.scrollableArea);
+			base.isWindowScrolling = base.$scrollableArea[0] === window;
+		};
+
+		base.updateOptions = function (options) {
+			base.setOptions(options);
+			// scrollableArea might have changed
+			base.unbind();
+			base.bind();
 			base.updateWidth();
 			base.toggleHeaders();
 		};
@@ -170,7 +268,7 @@
 		return this.each(function () {
 			var instance = $.data(this, 'plugin_' + name);
 			if (instance) {
-				if (typeof options === "string") {
+				if (typeof options === 'string') {
 					instance[options].apply(instance);
 				} else {
 					instance.updateOptions(options);
