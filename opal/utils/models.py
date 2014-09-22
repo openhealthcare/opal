758421fbb0f651aefcff89c0e8ecd0d8e0d1fb02
David Miller <david@deadpansincerity.com>
2014-09-22 11:48:58 +0100

    Add generic roles to user profiles refs openhealthcare/elcid#291

--- opal/utils/models.py
+++ opal/utils/models.py
@@ -17,13 +17,14 @@ def lookup_list(name, module=__name__):
     Given the name of a lookup list, return the tuple of class_name, bases, attrs
     for the user to define the class
     """
+    prefix = 'Lookup List: '
     class_name = name.capitalize() # TODO handle camelcase properly
     bases = (LookupList,)
     attrs = {
         'name': models.CharField(max_length=255, unique=True),
         'synonyms': generic.GenericRelation('opal.Synonym'),
         'Meta': type('Meta', (object,), {'ordering': ['name'], 
-                                         'verbose_name': 'Lookup List: '+name}),
+                                         'verbose_name': prefix+name}),
         '__unicode__': lambda self: self.name,
         '__module__': module,
     }
