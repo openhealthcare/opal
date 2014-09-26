41dc6673d23045dfcc091b806adad9dad9330227
David Miller <david@deadpansincerity.com>
2014-09-26 17:39:10 +0100

    First pass at dummy ward rounds, splits some javascript moduling stuff out, snippetises.

--- opal/static/js/opal/services_module.js
+++ opal/static/js/opal/services_module.js
@@ -3,5 +3,4 @@ var CATEGORIES = [
     'Inepisode', 'Review', 'Followup', 'Transferred', 'Discharged', 'Deceased'
 ];
 
-var services = angular.module('opal.services', ['ngResource', 'ngRoute']);
-services.config(function($sceProvider){$sceProvider.enabled(false)});
+var services = OPAL.module('opal.services', ['ngResource', 'ngRoute']);
