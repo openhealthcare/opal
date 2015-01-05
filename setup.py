a4b47cf6d22211b4573ce2628f214d79c4da25fc
David Miller <david@deadpansincerity.com>
2015-01-05 21:57:15 +0000

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='opal',
-    version='0.1',
+    version='0.3.0',
     packages=['opal', 'opal.utils'],
     include_package_data=True,
     license='GPL3',  # example license
