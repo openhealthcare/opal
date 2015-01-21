5c445b6db712e444778783cc602e1b98b16fe25c
David Miller <david@deadpansincerity.com>
2015-01-21 10:04:52 +0000

    Add Startplugin to our management script.
    refs #248

---
+++ bin/plugin_scaffold/app/urls.py
@@ -0,0 +1,11 @@
+"""
+Urls for the {{ name }} OPAL plugin
+"""
+from django.conf.urls import patterns, url
+
+from {{ name }} import views
+
+urlpatterns = patterns(
+    '',
+    #url(pattern, view)
+)
