3e48a357744248b8964801c6547ba4db2b4ac1ae
David Miller <david@deadpansincerity.com>
2015-04-18 21:20:53 +0100

    Don't need to generate those urls any more

--- opal/urls.py
+++ opal/urls.py
@@ -97,11 +97,6 @@ for subrecord_model in subrecords():
     sub_url = camelcase_to_underscore(subrecord_model.__name__)
     urlpatterns += patterns(
         '',
-        # url(r'^%s/?$' % sub_url, views.subrecord_create_view,
-        #     {'model': subrecord_model}),
-        # url(r'^%s/(?P<pk>\d+)/?$' % sub_url,
-        #     views.subrecord_detail_view, {'model': subrecord_model}),
-
         url(r'^templates/modals/%s.html/?$' % sub_url,
             views.ModalTemplateView.as_view(), {'model': subrecord_model}),
         url(r'^templates/modals/%s.html/(?P<tag>[a-z_\-]+)/?$' % sub_url, 
