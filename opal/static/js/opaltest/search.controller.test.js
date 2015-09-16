f08dc991c0f2aadf15fb2fcc90b3d3cc30bbab71
fredkingham <fredkingham@gmail.com>
2015-09-16 15:27:49 +0100

    adding additional tests to test the new search on any page feature openhealthcare/elcid#285

--- opal/static/js/opaltest/search.controller.test.js
+++ opal/static/js/opaltest/search.controller.test.js
@@ -1,14 +1,14 @@
 describe('SearchCtrl', function (){
     var $scope, location;
     var Episode, Flow;
-    var profile, schema, options;
+    var profile, schema, options, locationDetails;
 
     beforeEach(function(){
         module('opal', function($provide) {
             $provide.value('$analytics', function(){
                 return {
                     pageTrack: function(x){}
-                }
+                };
             });
 
             $provide.provider('$analytics', function(){
@@ -25,41 +25,42 @@ describe('SearchCtrl', function (){
         });
     });
 
-    beforeEach(function(){ module('opal.controllers') });
+    beforeEach(module('opal.controllers'));
 
-    beforeEach(inject(function($injector){
+    beforeEach(function(){
+        locationDetails = {};
+        $window = {location: locationDetails };
+
+        module(function($provide) {
+          $provide.value('$window', $window);
+        });
+
+        inject(function($injector){
+
         $rootScope   = $injector.get('$rootScope');
         $scope       = $rootScope.$new();
         $controller  = $injector.get('$controller');
-        location     = $injector.get('$location');
         Episode      = $injector.get('Episode');
         Flow         = $injector.get('Flow');
         $httpBackend = $injector.get('$httpBackend');
-        $location = $injector.get('$location');
-
-        $location.search({
-            hospital_number: "Bond",
-            name: "Bond",
-            page_number: 1
-        })
+        location = $injector.get('$location');
+        $window = $injector.get('$window');
 
         schema  = {};
         options = {};
         profile = {};
-
 
         controller = $controller('SearchCtrl', {
             $scope         : $scope,
             $location      : location,
             Episode: Episode,
             Flow: Flow,
-
             options        : options,
             schema         : schema,
             profile        : profile
         });
 
-    }));
+    });});
 
     afterEach(function() {
         $httpBackend.verifyNoOutstandingExpectation();
@@ -68,6 +69,12 @@ describe('SearchCtrl', function (){
 
     describe('We should query for hospital number or name()', function (){
         it('should ask the server for results', function(){
+            location.search({
+                hospital_number: "Bond",
+                name: "Bond",
+                page_number: 1
+            });
+
             expectedUrl = "/search/simple/?hospital_number=Bond&name=Bond&page_number=1";
             $httpBackend.expectGET(expectedUrl).respond({
                 page_number: 1,
@@ -77,7 +84,30 @@ describe('SearchCtrl', function (){
             $scope.loadResults();
             $httpBackend.flush();
         });
+
+        it("should redirect to the search page if we're not in search", function(){
+            locationDetails.href = "";
+            locationDetails.pathname = "/somewhere";
+            $scope.searchTerm = "Bond";
+            $scope.search();
+            expectedUrl = "/#/search?hospital_number=Bond&name=Bond";
+            expect(locationDetails.href).toEqual(expectedUrl);
+        });
+
+        it("should update the url if on the search page", function(){
+            locationDetails.href = "unchanged";
+            locationDetails.pathname = "/";
+            $scope.searchTerm = "Bond";
+            $scope.search();
+            expectedSearch = {
+                hospital_number: "Bond",
+                name: "Bond",
+            };
+            expect(location.search()).toEqual(expectedSearch);
+            expect(locationDetails.href).toEqual("unchanged");
+        });
     });
+
 
     describe('jumpToEpisode()', function (){
         it('Should call location.path()', function () {
