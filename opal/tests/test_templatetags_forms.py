ef6f7a1be0c52b350cb5aab7d4694097e267467a
fredkingham <fredkingham@gmail.com>
2015-10-13 15:01:36 +0100

    fixing unit test breaks caused by the merge with 0.4.3

--- opal/tests/test_templatetags_forms.py
+++ opal/tests/test_templatetags_forms.py
@@ -9,29 +9,31 @@ from opal.templatetags.forms import input, select, textarea
 class TextareaTest(TestCase):
     def setUp(self):
         self.template = Template('{% load forms %}{% textarea label="hai" model="bai"%}')
-        
+
     def test_textarea(self):
         rendered = self.template.render(Context({}))
         self.assertIn('ng-model="bai"', rendered)
         self.assertIn('hai', rendered)
-        
+
 
 class SelectTest(TestCase):
     def setUp(self):
         self.template = Template('{% load forms %}{% select label="hai" model="bai"%}')
-        
+
     def test_select(self):
         rendered = self.template.render(Context({}))
         self.assertIn('ng-model="bai"', rendered)
-        self.assertIn('hai</label>', rendered)
-        
+
+        # remove white space
+        cleaned = "".join([i.strip() for i in rendered.split("\n") if i.strip()])
+        self.assertIn('<label class="control-label col-sm-3">hai</label>', cleaned)
+
 
 class InputTest(TestCase):
     def setUp(self):
         self.template = Template('{% load forms %}{% input label="hai" model="bai"%}')
-        
+
     def test_input(self):
         rendered = self.template.render(Context({}))
         self.assertIn('ng-model="bai"', rendered)
         self.assertIn('hai', rendered)
-        
