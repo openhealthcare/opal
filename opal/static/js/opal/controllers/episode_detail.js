108f5c617dd88ffa05473e9d162a1c6a078fc3cc
fredkingham <fredkingham@gmail.com>
2015-11-05 13:57:21 +0000

    move logic into a function

--- opal/static/js/opal/controllers/episode_detail.js
+++ opal/static/js/opal/controllers/episode_detail.js
@@ -3,14 +3,16 @@ angular.module('opal.controllers').controller(
                                   $rootScope,
                                   Flow, EpisodeDetailMixin,
                                   episode, options, profile) {
-        $scope._ = _;
-	    $rootScope.state = 'normal';
-        $scope.url = $location.url();
-        $scope.Flow = Flow;
-	    $scope.episode = episode;
-        $scope.options = options;
-        $scope.profile =  profile;
-        $scope.tag_display = options.tag_display;
-        $scope.tag_visibile_in_list = options.tag_visibile_in_list;
-        EpisodeDetailMixin($scope);
+          $scope._ = _;
+    	    $rootScope.state = 'normal';
+          $scope.url = $location.url();
+          $scope.Flow = Flow;
+    	    $scope.episode = episode;
+          $scope.options = options;
+          $scope.profile =  profile;
+          $scope.tag_display = options.tag_display;
+          $scope.is_tag_visibile_in_list = function(tag){
+              return _.contains(options.tag_visibile_in_list, tag);
+          };
+          EpisodeDetailMixin($scope);
     });
