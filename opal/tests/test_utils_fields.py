aa5d9c3c9155e60747005a7c4893fae812ca47c9
fredkingham <fredkingham@gmail.com>
2015-11-30 17:22:03 +0000

    updating the troublesome leak

--- opal/tests/test_utils_fields.py
+++ opal/tests/test_utils_fields.py
@@ -10,13 +10,13 @@ from opal.core import lookuplists
 class FKorFTTest(TestCase):
     def setUp(self):
         class LookupList(lookuplists.LookupList): pass
-        
+
         class Model(models.Model):
             field = ForeignKeyOrFreeText(LookupList)
-            
+
         self.Model = Model
         self.ll = LookupList
-        
+
     def test_set_none(self):
         # As much as anything this is checking that we *can* set None
         # on a FKorFT field - ensuring a previous bug where .split()
