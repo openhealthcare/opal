eda932b41d7f21cf425209b070cebd312f7db9a6
fredkingham <fredkingham@gmail.com>
2015-12-10 21:49:45 +0000

    update patient summaries

--- opal/static/js/opal/services/patient_summary.js
+++ opal/static/js/opal/services/patient_summary.js
@@ -4,15 +4,16 @@
 angular.module('opal.services').factory('PatientSummary', function(UserProfile) {
         var PatientSummary = function(jsonResponse){
             var startYear, endYear;
+            var DATE_FORMAT = "DD/MM/YYYY"
             var self = this;
 
             if(jsonResponse.start_date && jsonResponse.end_date){
                 if(jsonResponse.start_year){
-                    startYear= moment(jsonResponse.start_date, 'YYYY-MM-DD').format("YYYY");
+                    startYear= moment(jsonResponse.start_date, DATE_FORMAT).format("YYYY");
                 }
 
                 if(jsonResponse.end_date){
-                    endYear = moment(jsonResponse.end_date, 'YYYY-MM-DD').format("YYYY");
+                    endYear = moment(jsonResponse.end_date, DATE_FORMAT).format("YYYY");
                 }
             }
 
@@ -24,7 +25,7 @@ angular.module('opal.services').factory('PatientSummary', function(UserProfile) 
             }
             this.name = jsonResponse.name;
             this.count = jsonResponse.count;
-            this.dateOfBirth = moment(jsonResponse.date_of_birth, 'YYYY-MM-DD');
+            this.dateOfBirth = moment(jsonResponse.date_of_birth, DATE_FORMAT);
             this.categories = jsonResponse.categories.join(", ");
             this.link = "#/episode/" + jsonResponse.episode_id;
             this.patientNotesLink = "#/patient/" + jsonResponse.id;
