d6e241a2eed1639ddfb311514c5d70260456e6fa
David Miller <david@deadpansincerity.com>
2016-02-28 00:20:31 +0000

    Interim commit to get the Gloss integration working

--- opal/core/glossolalia/api.py
+++ opal/core/glossolalia/api.py
@@ -1,19 +1,38 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from models import GlossolaliaSubscription
-from opal.core.api import OPALRouter
 
-router = OPALRouter()
+from opal.core.subrecords import subrecords
+from opal.core.api import router
+from opal.core.views import _get_request_data, _build_json_response
+from opal.utils import camelcase_to_underscore
 
-class GlossolaliaViewSet(viewsets.ViewSet):
-    base_name = 'glossolalia'
+from opal.core.glossolalia.models import GlossolaliaSubscription
 
-    def list(self, *args, **kwargs):
-        queryset = GlossolaliaSubscription.objects.all()
-        serialised = queryset.values(*GlossolaliaSubscription.SERIALISED_FIELDS)
-        return Response(serialised)
+# class GlossolaliaViewSet(viewsets.ViewSet):
+#     base_name = 'glossolalia'
 
-    def update(self, request, pk=None): pass
+#     def list(self, *args, **kwargs):
+#         queryset = GlossolaliaSubscription.objects.all()
+#         serialised = queryset.values(*GlossolaliaSubscription.SERIALISED_FIELDS)
+#         return Response(serialised)
 
+#     def update(self, request, pk=None): pass
 
-router.register('glossolalia', GlossolaliaViewSet)
+class GlossSubrecordViewSet(viewsets.ViewSet):
+    base_name = 'glosssubrecord'
+
+    def create(self, request):
+        identifier = request.data['identifier']
+        data = request.data['data']
+        self.model.create_or_update_from_gloss(identifier, data)
+        return _build_json_response('Hai')
+
+#router.register('glossolalia', GlossolaliaViewSet)
+for sub in subrecords():
+    sub_name = camelcase_to_underscore(sub.__name__)
+
+    class SubViewSet(GlossSubrecordViewSet):
+        base_name = 'gloss' + sub_name
+        model = sub
+
+    router.register('gloss' + sub_name, SubViewSet)
