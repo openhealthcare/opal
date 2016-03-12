9a59591dac57ba9d82af39f2431966a8371fcb9b
David Miller <david@deadpansincerity.com>
2016-03-12 14:40:16 +0000

    Timeline panels should have edit/add buttons that actually render.

--- opal/tests/test_panels.py
+++ opal/tests/test_panels.py
@@ -22,3 +22,22 @@ class RecordPanelTestCase(OpalTestCase):
         )
         result = panels.record_panel(Demographics())
         self.assertEqual(expected, result)
+
+
+class RecordTimelineTestCase(OpalTestCase):
+    def test_record_timeline(self):
+        expected = dict(
+            name='demographics',
+            title='Demographics',
+            detail_template='records/demographics.html',
+            icon=None,
+            editable=True,
+            whenfield='when'
+        )
+        result = panels.record_timeline(Demographics(), 'when')
+        self.assertEqual(expected, result)
+
+
+class TemasPanelTestCase(OpalTestCase):
+    def test_teams_panel(self):
+        self.assertEqual({}, panels.teams_panel())
