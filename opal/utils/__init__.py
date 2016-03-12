41b1060a674fd2c9c8a9de3e6340f32d5b8c85d0
David Miller <david@deadpansincerity.com>
2016-03-12 20:46:24 +0000

    Remove unused import found by Landscape

--- opal/utils/__init__.py
+++ opal/utils/__init__.py
@@ -5,8 +5,6 @@ import importlib
 import os
 import re
 
-from django.template import engines, TemplateDoesNotExist
-
 camelcase_to_underscore = lambda str: re.sub('(((?<=[a-z])[A-Z])|([A-Z](?![A-Z]|$)))', '_\\1', str).lower().strip('_')
 
 def stringport(module):
