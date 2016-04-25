fcc48724739662a1c946d5b6d1882343dc4a764c
fredkingham <fredkingham@gmail.com>
2016-04-25 23:31:40 +0100

    fixes #999 the problem was that during the time it takes options to load it was showing the 404 page. Solution create an empty page template. We need to use an empty page template rather than just opal.html because the outer-container has a min height on it that shows as a grey rectangle while options are loading on firefox

--- opal/static/js/opal/controllers/patient_list_redirect.js
+++ opal/static/js/opal/controllers/patient_list_redirect.js
@@ -2,15 +2,16 @@ angular.module('opal.controllers').controller(
     'PatientListRedirectCtrl', function($scope, $cookieStore, $location, options){
         "use strict";
         // a simple controller that redirects to the correct tag/subtag
+        var target;
         $scope.ready = false;
 
         var path_base = '/list/';
         var last_list = $cookieStore.get('opal.lastPatientList');
         if(last_list){
-            var target = path_base + last_list;
+            target = path_base + last_list;
         }else{
-            var target = path_base + options.first_list_slug;
+            target = path_base + options.first_list_slug;
         }
-        $location.path( target + '/');
+        $location.path(target + '/');
         $location.replace();
 });
