0bb4458431b9c6d505f11d1493ae0f1f64408c09
fredkingham <fredkingham@gmail.com>
2016-04-26 14:45:42 +0100

    this is getting rediculous

--- opal/static/js/opal/controllers/patient_list_redirect.js
+++ opal/static/js/opal/controllers/patient_list_redirect.js
@@ -1,23 +1,24 @@
 angular.module('opal.controllers').controller(
   'PatientListRedirectCtrl', function($scope, $cookieStore, $location, options){
   "use strict";
-  // a simple controller that redirects to the correct tag/subtag
-  // var target;
-  // $scope.ready = false;
-  //
-  // var replacePath = function(last_list){
-  //   var path_base = '/list/';
-  //   target = path_base + last_list;
-  //   $location.path(target + '/');
-  //   $location.replace();
-  // };
-  //
-  // var last_list = $cookieStore.get('opal.lastPatientList');
-  // if(last_list){
-  //   replacePath(last_list);
-  // }else{
-  //   Options.then(function(options){
-  //     replacePath(options.first_list_slug);
-  //   });
-  // }
+  a simple controller that redirects to the correct tag/subtag
+  var target;
+  $scope.ready = false;
+
+  var replacePath = function(last_list){
+    var path_base = '/list/';
+    target = path_base + last_list;
+    $location.path(target + '/');
+    $location.replace();
+  };
+
+  var last_list = $cookieStore.get('opal.lastPatientList');
+  if(last_list){
+    replacePath(last_list);
+  }else{
+    replacePath(options.first_list_slug);
+    // Options.then(function(options){
+    //   replacePath(options.first_list_slug);
+    // });
+  }
 });
