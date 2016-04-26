70ca23e8faa8e7d47a3f10c7e16b09e86db0aea2
fredkingham <fredkingham@gmail.com>
2016-04-26 12:47:22 +0100

    trying this

--- opal/static/js/opal/controllers/patient_list_redirect.js
+++ opal/static/js/opal/controllers/patient_list_redirect.js
@@ -1,23 +1,23 @@
 angular.module('opal.controllers').controller(
-  'PatientListRedirectCtrl', function($scope, $cookieStore, $location, Options){
+  'PatientListRedirectCtrl', function($scope){
   "use strict";
   // a simple controller that redirects to the correct tag/subtag
-  var target;
-  $scope.ready = false;
-
-  var replacePath = function(last_list){
-    var path_base = '/list/';
-    target = path_base + last_list;
-    $location.path(target + '/');
-    $location.replace();
-  };
-
-  var last_list = $cookieStore.get('opal.lastPatientList');
-  if(last_list){
-    replacePath(last_list);
-  }else{
-    Options.then(function(options){
-      replacePath(options.first_list_slug);
-    });
-  }
+  // var target;
+  // $scope.ready = false;
+  //
+  // var replacePath = function(last_list){
+  //   var path_base = '/list/';
+  //   target = path_base + last_list;
+  //   $location.path(target + '/');
+  //   $location.replace();
+  // };
+  //
+  // var last_list = $cookieStore.get('opal.lastPatientList');
+  // if(last_list){
+  //   replacePath(last_list);
+  // }else{
+  //   Options.then(function(options){
+  //     replacePath(options.first_list_slug);
+  //   });
+  // }
 });
