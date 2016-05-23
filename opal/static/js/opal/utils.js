ef802384c049a3507b4c17cfea854d63fdddef5f
fredkingham <fredkingham@gmail.com>
2016-05-23 15:07:50 +0100

    a different method, ie is unfocussing on backspace

--- opal/static/js/opal/utils.js
+++ opal/static/js/opal/utils.js
@@ -188,30 +188,11 @@ if (typeof String.prototype.trim !== 'function') {
 
 // Prevent the backspace key from navigating back.
 $(document).unbind('keydown').bind('keydown', function (event) {
-    var doPrevent = false;
-    if (event.keyCode === 8) {
-        var d = event.srcElement || event.target;
-        if ((d.tagName.toUpperCase() === 'INPUT' &&
-             (
-                 d.type.toUpperCase() === 'TEXT' ||
-                 d.type.toUpperCase() === 'PASSWORD' ||
-                 d.type.toUpperCase() === 'FILE' ||
-                 d.type.toUpperCase() === 'SEARCH' ||
-                 d.type.toUpperCase() === 'EMAIL' ||
-                 d.type.toUpperCase() === 'NUMBER' ||
-                 d.type.toUpperCase() === 'DATE' )
-             ) ||
-             d.tagName.toUpperCase() === 'TEXTAREA') {
-            doPrevent = d.readOnly || d.disabled;
+    $(document).on("keydown", function (e) {
+        if (e.which === 8 && !$(e.target).is("input:not([readonly]):not([type=radio]):not([type=checkbox]), textarea, [contentEditable], [contentEditable=true]")) {
+            e.preventDefault();
         }
-        else {
-            doPrevent = true;
-        }
-    }
-
-    if (doPrevent) {
-        event.preventDefault();
-    }
+    });
 });
 
 
