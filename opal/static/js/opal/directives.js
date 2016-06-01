e62d77db902158ceb746ba0f1b30d6dd18039cb7
fredkingham <fredkingham@gmail.com>
2016-06-01 16:45:54 +0100

    remove unused directive

--- opal/static/js/opal/directives.js
+++ opal/static/js/opal/directives.js
@@ -232,33 +232,6 @@ directives.directive('autofocus', ['$timeout', function($timeout) {
   };
 }]);
 
-directives.directive('moDateInput', function ($window) {
-    return {
-        require:'^ngModel',
-        restrict:'A',
-        link:function (scope, elm, attrs, ctrl) {
-            var moment = $window.moment;
-            var dateFormat = attrs.moDateInput;
-            attrs.$observe('moDateInput', function (newValue) {
-                if (dateFormat == newValue || !ctrl.$modelValue) return;
-                dateFormat = newValue;
-                ctrl.$modelValue = new Date(ctrl.$setViewValue);
-            });
-
-            ctrl.$formatters.unshift(function (modelValue) {
-                if (!dateFormat || !modelValue) return "";
-                var retVal = moment(modelValue).format(dateFormat);
-                return retVal;
-            });
-
-            ctrl.$parsers.unshift(function (viewValue) {
-                var date = moment(viewValue, dateFormat);
-                return (date && date.isValid() && date.year() > 1950 ) ? date.toDate() : "";
-            });
-        }
-    };
-});
-
 directives.directive("dateOfBirth", function(){
   return {
     require: "?ngModel",
