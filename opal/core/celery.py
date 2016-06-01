f96a6e66486cf582be133e1b822a159f4a53073b
David Miller <david@deadpansincerity.com>
2016-06-01 11:44:39 +0100

    Delay task and set UID on signal handler

--- opal/core/celery.py
+++ opal/core/celery.py
@@ -16,7 +16,6 @@ app = Celery('proj')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
