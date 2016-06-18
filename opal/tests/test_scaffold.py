b652204700c7da0a6a56f6ffbe7c1575fd1a9c12
fredkingham <fredkingham@gmail.com>
2016-06-19 00:08:43 +0100

    start to add tests

--- opal/tests/test_scaffold.py
+++ opal/tests/test_scaffold.py
@@ -11,25 +11,45 @@ from opal.core.scaffold import (
 )
 
 
-# @patch("ffs.Path.__lshift__")
-# class FormRenderTestCase(OpalTestCase):
-#     def test_form_render(self, lshift):
-#         scaffold_path = ffs.Path(settings.PROJECT_PATH)/'scaffolding'
-#         create_form_template_for(Colour, scaffold_path)
-#         lshift.assert_called_once_with(
-#             '{% load forms %}\n{% input  field="Colour.name"  %}'
-#         )
-
-# @patch("ffs.Path.__lshift__")
+@patch("ffs.Path.__lshift__")
 class FormRenderTestCase(OpalTestCase):
-    def test_form_render(self):
+    def test_form_render(self, lshift):
+        """ test a generic string render
+        """
         scaffold_path = ffs.Path(settings.PROJECT_PATH)/'scaffolding'
         create_form_template_for(Colour, scaffold_path)
+        lshift.assert_called_once_with(
+            '{% load forms %}\n{% input  field="Colour.name"  %}'
+        )
+
+    @patch.object(Colour, "build_field_schema")
+    def test_date_render(self, lshift, build_field_schema):
+        pass
+
+    @patch.object(Colour, "build_field_schema")
+    def test_datetime_render(self, build_field_schema, lshift):
+        build_field_schema.return_value = {
+            'lookup_list': None,
+            'model': 'Colour',
+            'name': 'name',
+            'title': 'Name',
+            'type': 'date_time'
+        },
+        scaffold_path = ffs.Path(settings.PROJECT_PATH)/'scaffolding'
+        create_form_template_for(Colour, scaffold_path)
+        lshift.assert_called_once_with(
+            '{% load forms %}\n{% input  field="Colour.name"  %}'
+        )
+
+    def test_boolean_render(self, lshift):
+        pass
 
 
 @patch("ffs.Path.__lshift__")
 class RecordRenderTestCase(OpalTestCase):
     def test_form_render(self, lshift):
+        """ test a generic string render
+        """
         scaffold_path = ffs.Path(settings.PROJECT_PATH)/'scaffolding'
         create_display_template_for(Colour, scaffold_path)
         lshift.assert_called_once_with(
