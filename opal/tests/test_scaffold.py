1564ff17d55078f0dbaf78d11c4b3656438ec1ce
fredkingham <fredkingham@gmail.com>
2016-06-19 00:23:36 +0100

    add full unit tests for records

--- opal/tests/test_scaffold.py
+++ opal/tests/test_scaffold.py
@@ -22,9 +22,17 @@ class FormRenderTestCase(OpalTestCase):
             '{% load forms %}\n{% input  field="Colour.name"  %}'
         )
 
-    @patch.object(Colour, "build_field_schema")
-    def test_date_render(self, lshift, build_field_schema):
-        pass
+
+@patch("ffs.Path.__lshift__")
+class RecordRenderTestCase(OpalTestCase):
+    def test_form_render(self, lshift):
+        """ test a generic string render
+        """
+        scaffold_path = ffs.Path(settings.PROJECT_PATH)/'scaffolding'
+        create_display_template_for(Colour, scaffold_path)
+        lshift.assert_called_once_with(
+            '<span ng-show="item.name">\n    [[ item.name ]]\n   <br />\n</span>'
+        )
 
     @patch.object(Colour, "build_field_schema")
     def test_datetime_render(self, build_field_schema, lshift):
@@ -36,22 +44,52 @@ class FormRenderTestCase(OpalTestCase):
             'type': 'date_time'
         },
         scaffold_path = ffs.Path(settings.PROJECT_PATH)/'scaffolding'
-        create_form_template_for(Colour, scaffold_path)
+        create_display_template_for(Colour, scaffold_path)
         lshift.assert_called_once_with(
-            '{% load forms %}\n{% input  field="Colour.name"  %}'
+            '<span ng-show="item.name">\n    [[ item.name | shortDateTime ]]\n   <br />\n</span>'
         )
 
-    def test_boolean_render(self, lshift):
-        pass
-
-
-@patch("ffs.Path.__lshift__")
-class RecordRenderTestCase(OpalTestCase):
-    def test_form_render(self, lshift):
-        """ test a generic string render
-        """
+    @patch.object(Colour, "build_field_schema")
+    def test_date_render(self, build_field_schema, lshift):
+        build_field_schema.return_value = {
+            'lookup_list': None,
+            'model': 'Colour',
+            'name': 'name',
+            'title': 'Name',
+            'type': 'date'
+        },
         scaffold_path = ffs.Path(settings.PROJECT_PATH)/'scaffolding'
         create_display_template_for(Colour, scaffold_path)
         lshift.assert_called_once_with(
-            '<span ng-show="item.name">\n    [[ item.name ]]\n   <br />\n</span>'
+            '<span ng-show="item.name">\n    [[ item.name | shortDate ]]\n   <br />\n</span>'
+        )
+
+    @patch.object(Colour, "build_field_schema")
+    def test_boolean_render(self, build_field_schema, lshift):
+        build_field_schema.return_value = {
+            'lookup_list': None,
+            'model': 'Colour',
+            'name': 'name',
+            'title': 'Name',
+            'type': 'boolean'
+        },
+        scaffold_path = ffs.Path(settings.PROJECT_PATH)/'scaffolding'
+        create_display_template_for(Colour, scaffold_path)
+        lshift.assert_called_once_with(
+            '<span ng-show="item.name">\n     Name\n   <br />\n</span>'
+        )
+
+    @patch.object(Colour, "build_field_schema")
+    def test_null_boolean_render(self, build_field_schema, lshift):
+        build_field_schema.return_value = {
+            'lookup_list': None,
+            'model': 'Colour',
+            'name': 'name',
+            'title': 'Name',
+            'type': 'null_boolean'
+        },
+        scaffold_path = ffs.Path(settings.PROJECT_PATH)/'scaffolding'
+        create_display_template_for(Colour, scaffold_path)
+        lshift.assert_called_once_with(
+            '<span ng-show="item.name">\n     Name\n   <br />\n</span>'
         )
