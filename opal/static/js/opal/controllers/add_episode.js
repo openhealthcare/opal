f3ac7fb8b21c1fc38051e824e5916bc5356c0d96
David Miller <david@deadpansincerity.com>
2016-06-25 18:07:37 +0100

    Indentation for add_episode.js

--- opal/static/js/opal/controllers/add_episode.js
+++ opal/static/js/opal/controllers/add_episode.js
@@ -8,32 +8,32 @@ angular.module('opal.controllers')
                                            options,
                                            demographics,
                                            tags) {
-      var currentTags = [];
+        var currentTags = [];
 
 	    for (var name in options) {
 		    $scope[name + '_list'] = options[name];
 	    };
 
 	    $scope.editing = {
-        tagging: [{}],
+            tagging: [{}],
 		    location: {},
-        demographics: demographics
+            demographics: demographics
 	    };
 
-      if(tags.tag){
-        currentTags = [tags.tag];
-      }
+        if(tags.tag){
+            currentTags = [tags.tag];
+        }
 
-      if(tags.subtag){
-        // if there's a subtag, don't tag with the parent tag
-        currentTags = [tags.subtag];
-      }
+        if(tags.subtag){
+            // if there's a subtag, don't tag with the parent tag
+            currentTags = [tags.subtag];
+        }
 
-      $scope.tagService = new TagService(currentTags);
+        $scope.tagService = new TagService(currentTags);
 
 	    $scope.save = function() {
-        $scope.editing.tagging = [$scope.tagService.toSave()];
-        var toSave = FieldTranslater.jsToPatient($scope.editing)
+            $scope.editing.tagging = [$scope.tagService.toSave()];
+            var toSave = FieldTranslater.jsToPatient($scope.editing)
 
 		    $http.post('/api/v0.1/episode/', toSave).success(function(episode) {
 			    episode = new Episode(episode);
