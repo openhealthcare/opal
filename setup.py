561d84b6bbc3e36dd6054c306c2fe89d3e9141c9
David Miller <david@deadpansincerity.com>
2016-06-28 14:29:25 +0100

    Removes: Fabric, cryptography, pycrypto, cffi, paramiko, pycparser, enum, idna, ipaddress, pyasn1 from the python environment post install.

--- setup.py
+++ setup.py
@@ -52,8 +52,6 @@ setup(
         'djangorestframework==3.2.2',
         'django-compressor==1.5',
         'python-dateutil==2.4.2',
-        'Fabric==1.10.2',
-        'pycrypto==2.6.1',
         'django-celery==3.1.17',
         'celery==3.1.19',
         ]
