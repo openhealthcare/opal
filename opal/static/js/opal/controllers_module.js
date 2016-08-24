6f2684d5bf6606d0aebed63b64703051dafe6215
David Miller <david@deadpansincerity.com>
2016-08-24 17:39:15 +0100

    Temporary commenting - remove these later

--- opal/static/js/opal/controllers_module.js
+++ opal/static/js/opal/controllers_module.js
@@ -4,14 +4,14 @@ var controllers = OPAL.module('opal.controllers', [
 	'ui.event',
 	'ui.bootstrap',
     'ngProgressLite',
-    'mgcrea.ngStrap.typeahead',
-    'mgcrea.ngStrap.helpers.dimensions',
-    'mgcrea.ngStrap.helpers.parseOptions',
-    'mgcrea.ngStrap.tooltip',
-    'mgcrea.ngStrap.popover',
-    'mgcrea.ngStrap.helpers.dateParser',
-    'mgcrea.ngStrap.datepicker',
-    'mgcrea.ngStrap.timepicker',
+    // 'mgcrea.ngStrap.typeahead',
+    // 'mgcrea.ngStrap.helpers.dimensions',
+    // 'mgcrea.ngStrap.helpers.parseOptions',
+    // 'mgcrea.ngStrap.tooltip',
+    // 'mgcrea.ngStrap.popover',
+    // 'mgcrea.ngStrap.helpers.dateParser',
+    // 'mgcrea.ngStrap.datepicker',
+    // 'mgcrea.ngStrap.timepicker',
     'ui.select'
 ]);
 
@@ -25,12 +25,12 @@ controllers.controller('RootCtrl', function($scope, $location) {
     if(typeof collaborator != 'undefined'){ collaborator($scope) };
 });
 
-controllers.config(function($datepickerProvider) {
+// controllers.config(function($datepickerProvider) {
 
-  angular.extend($datepickerProvider.defaults, {
-      autoclose: true,
-      dateFormat: 'dd/MM/yyyy',
-      dateType: 'string'
-  });
+//   angular.extend($datepickerProvider.defaults, {
+//       autoclose: true,
+//       dateFormat: 'dd/MM/yyyy',
+//       dateType: 'string'
+//   });
 
-})
+// })
