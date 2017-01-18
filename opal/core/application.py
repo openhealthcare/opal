4921a562d2162d06959611ba03f484fcb170c878
David Miller <david@deadpansincerity.com>
2017-01-18 13:52:12 +0000

    We have no need for templates in .js files. That's stupid.

--- opal/core/application.py
+++ opal/core/application.py
@@ -72,7 +72,6 @@ class OpalApplication(object):
             'js/opal/services/metadata.js',
             'js/opal/services/patient_consultation_record.js',
             "js/search/services/paginator.js",
-            "js/search/templates/search_result.js",
         ],
         'opal.controllers': [
             "js/opal/controllers_module.js",
