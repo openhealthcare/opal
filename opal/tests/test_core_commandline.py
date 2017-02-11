54e78b61db2660a57762b0f0115d532b308386e4
David Miller <david@deadpansincerity.com>
2017-02-11 04:36:07 +0000

    Add simple basic unittests for some of our commandline argparse target functions

--- opal/tests/test_core_commandline.py
+++ opal/tests/test_core_commandline.py
@@ -1,6 +1,28 @@
 """
 Unittests for opal.core.commandline
 """
+from mock import patch, MagicMock
+
 from opal.core.test import OpalTestCase
 
 from opal.core import commandline
+
+
+class StartprojectTestCase(OpalTestCase):
+
+    def test_startproject(self):
+        mock_args = MagicMock(name='Mock Args')
+        mock_args.name = 'projectname'
+        with patch.object(commandline.scaffold_utils, 'start_project') as sp:
+            commandline.startproject(mock_args)
+            sp.assert_called_with('projectname', commandline.USERLAND_HERE)
+
+
+class StartpluginTestCase(OpalTestCase):
+
+    def test_startplugin(self):
+        mock_args = MagicMock(name='Mock Args')
+        mock_args.name = 'pluginname'
+        with patch.object(commandline.scaffold_utils, 'start_plugin') as sp:
+            commandline.startplugin(mock_args)
+            sp.assert_called_with('pluginname', commandline.USERLAND_HERE)
