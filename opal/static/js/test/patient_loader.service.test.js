ccdb0e50b7c701f9c558f5e000bdd376c33b29f3
David Miller <david@deadpansincerity.com>
2017-02-17 23:25:28 +0000

    Add some tests for failure modes of our PatientLoader

--- opal/static/js/test/patient_loader.service.test.js
+++ opal/static/js/test/patient_loader.service.test.js
@@ -2,47 +2,87 @@
 // Unit tests for our patient loader service
 //
 describe('patientLoader', function() {
-  "use strict";
+    "use strict";
 
-  var $httpBackend, $route, $rootScope;
-  var patientLoader;
+    var $httpBackend, $route, $rootScope;
+    var mockWindow;
+    var patientLoader;
 
-  var response = {
-    demographics: [{first_name: "Sue", surname: "Jackson", patient_id: 1}],
-    episodes: {
-      122: {id: 122, start: "20/01/2016", end: "20/02/2016", demographics: [{first_name: "Sue", surname: "Jackson", patient_id: 1}]},
-      123: {id: 123, start: "20/01/2016", end: undefined, demographics: [{first_name: "Sue", surname: "Jackson", patient_id: 1}]},
-      124: {id: 124, start: undefined, end: "20/03/2016", demographics: [{first_name: "Sue", surname: "Jackson", patient_id: 1}]},
-      125: {id: 125, start: undefined, end: undefined, demographics: [{first_name: "Sue", surname: "Jackson", patient_id: 1}]}
-    }
-  };
+    var response = {
+        demographics: [{first_name: "Sue", surname: "Jackson", patient_id: 1}],
+        episodes: {
+            122: {
+                id: 122, start: "20/01/2016", end: "20/02/2016",
+                demographics: [{first_name: "Sue", surname: "Jackson", patient_id: 1}]},
+            123: {
+                id: 123, start: "20/01/2016", end: undefined,
+                demographics: [{first_name: "Sue", surname: "Jackson", patient_id: 1}]},
+            124: {
+                id: 124, start: undefined, end: "20/03/2016",
+                demographics: [{first_name: "Sue", surname: "Jackson", patient_id: 1}]},
+            125: {
+                id: 125, start: undefined, end: undefined,
+                demographics: [{first_name: "Sue", surname: "Jackson", patient_id: 1}]}
+        }
+    };
 
-  beforeEach(function(){
-    module('opal.services');
+    beforeEach(function(){
+        module('opal.services');
 
-    inject(function($injector){
-      patientLoader = $injector.get('patientLoader');
-      $httpBackend  = $injector.get('$httpBackend');
-      $route        = $injector.get('$route');
-      $rootScope    = $injector.get('$rootScope');
+        mockWindow = { alert: jasmine.createSpy() };
+
+        module(function($provide) {
+            $provide.value('$window', mockWindow);
+        });
+
+        inject(function($injector){
+            patientLoader = $injector.get('patientLoader');
+            $httpBackend  = $injector.get('$httpBackend');
+            $route        = $injector.get('$route');
+            $rootScope    = $injector.get('$rootScope');
+        });
+        $httpBackend.expectGET('/api/v0.1/record/').respond({});
+        $httpBackend.expectGET('/api/v0.1/userprofile/').respond({});
+        $route.current = { params: { patient_id: '123' } };
     });
 
-    $httpBackend.expectGET('/api/v0.1/record/').respond({});
-    $httpBackend.expectGET('/api/v0.1/userprofile/').respond({});
-    $httpBackend.expectGET('/api/v0.1/patient/123/').respond(response);
-    $route.current = { params: { patient_id: '123' } };
-  });
-
-  afterEach(function(){
-    $rootScope.$apply();
-    $httpBackend.flush();
-  });
-
-  describe('load patients', function() {
-    it('should load some patients', function() {
-      patientLoader().then(function(patient){
-        expect(patient.demographics[0].first_name).toEqual("Sue");
-      });
+    afterEach(function(){
+        $rootScope.$apply();
+        $httpBackend.flush();
     });
-  });
+
+    describe('load patients', function() {
+
+        beforeEach(function(){
+            $httpBackend.expectGET('/api/v0.1/patient/123/').respond(response);
+        })
+
+        it('should load some patients', function() {
+            patientLoader().then(function(patient){
+                expect(patient.demographics[0].first_name).toEqual("Sue");
+            });
+        });
+
+    });
+
+    describe('patient API errors', function() {
+
+        it('should alert() the user', function() {
+            $httpBackend.expectGET('/api/v0.1/patient/123/').respond(500);
+            patientLoader().then(function(patient){
+                expect(mockWindow.alert).toHaveBeenCalledWith('Patient could not be loaded');
+            });
+        });
+
+    });
+
+    describe('No patient id found', function() {
+        it('should fail quiently if we have no way of knowing the patient ID', function() {
+            $route.current.params = {};
+            patientLoader().then(function(p){
+                expect(p).toEqual([]);
+            })
+        });
+
+    });
 });
