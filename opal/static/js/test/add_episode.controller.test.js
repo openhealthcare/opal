54b821fda8527d529917243e5a486f649f5c69a2
fredkingham <fredkingham@gmail.com>
2017-03-03 15:36:55 +0000

    fix the casting of an episode date of admission when you add an episode

--- opal/static/js/test/add_episode.controller.test.js
+++ opal/static/js/test/add_episode.controller.test.js
@@ -4,49 +4,40 @@ describe('AddEpisodeCtrl', function (){
     var $scope, $httpBackend, $rootScope;
     var Schema
     var modalInstance;
-    var columns = {
-        "default": [
-            {
-                name: 'demographics',
-                single: true,
-                fields: [
-                    {name: 'first_name', type: 'string'},
-                    {name: 'surname', type: 'string'},
-                    {name: 'date_of_birth', type: 'date'},
-                ]},
-            {
-                name: 'location',
-                single: true,
-                fields: [
-                    {name: 'category', type: 'string'},
-                    {name: 'hospital', type: 'string'},
-                    {name: 'ward', type: 'string'},
-                    {name: 'bed', type: 'string'},
-                    {name: 'date_of_admission', type: 'date'},
-                    {name: 'tags', type: 'list'},
-                ]},
-            {
-                name: 'diagnosis',
-                single: false,
-                fields: [
-                    {name: 'condition', type: 'string'},
-                    {name: 'provisional', type: 'boolean'},
-                ]},
-        ]
+    var fields = {
+        'demographics': {
+            name: "demographics",
+            single: true,
+            fields: [
+                {name: 'name', type: 'string'},
+                {name: 'date_of_birth', type: 'date'},
+                {name: 'created', type: 'date_time'},
+            ],
+        },
+        "diagnosis": {
+            name: "diagnosis",
+            single: false,
+            sort: 'date_of_diagnosis',
+            fields: [
+                {name: 'date_of_diagnosis', type: 'date'},
+                {name: 'condition', type: 'string', default: 'flu'},
+                {name: 'provisional', type: 'boolean'},
+            ]
+        }
     };
 
     var referencedata = {
         dogs: ['Poodle', 'Dalmation'],
         hats: ['Bowler', 'Top', 'Sun']
     };
-    referencedata.toLookuplists = function(){
-        return { dog_list: referencedata.dogs, hats_list: referencedata.hats }
-    }
 
+    referencedata.toLookuplists = function(){
+        return { dog_list: referencedata.dogs, hats_list: referencedata.hats };
+    };
 
     beforeEach(function(){
         module('opal');
-        var $controller, $modal
+        var $controller, $modal;
         $scope = {};
 
         inject(function($injector){
@@ -56,9 +47,9 @@ describe('AddEpisodeCtrl', function (){
             Schema       = $injector.get('Schema');
             $rootScope   = $injector.get('$rootScope');
         });
-        $rootScope.fields = angular.copy(columns.default);
+        $rootScope.fields = fields;
 
-        var schema = new Schema(columns.default);
+        // var schema = new Schema(columns.default);
         modalInstance = $modal.open({template: 'Notatemplate'});
         $scope = $rootScope.$new();
 
@@ -77,11 +68,16 @@ describe('AddEpisodeCtrl', function (){
     });
 
     describe('save()', function(){
-
         it('should save', function(){
-            $httpBackend.expectPOST('/api/v0.1/episode/').respond({demographics:[{patient_id: 1}]})
-            $scope.editing.date_of_admission = moment(new Date(13, 1, 2014));
-            $scope.editing.demographics.date_of_birth = moment(new Date(13, 1, 1914));
+            var expected = {
+              "tagging":{"id":true,"id_inpatients":true},
+              "location":{},
+              "demographics":{"date_of_birth":"22/01/1970"},
+              "date_of_admission":"22/01/2000"
+            };
+            $httpBackend.expectPOST('/api/v0.1/episode/', expected).respond({demographics:[{patient_id: 1}]});
+            $scope.editing.date_of_admission = new Date(2000, 0, 22);
+            $scope.editing.demographics.date_of_birth = new Date(1970, 0, 22);
             $scope.save();
             $httpBackend.flush();
         });
