92921282ff176557f45592cfae4fa6f3e712bcef
fredkingham <fredkingham@gmail.com>
2017-03-13 14:20:45 +0000

    this is nice, we should really add get_absolute_url to the models

--- opal/tests/test_admin.py
+++ opal/tests/test_admin.py
@@ -1,13 +1,14 @@
-from opal.admin import LookupListForm
+from opal.admin import LookupListForm, PatientAdmin, EpisodeAdmin
 from opal.core.test import OpalTestCase
 from opal.tests.models import Hat
-from opal.models import Synonym
+from opal.models import Synonym, Patient, Episode
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.admin.sites import AdminSite
 
 
 class HatForm(LookupListForm):
     class Meta:
-        model=Hat
+        model = Hat
         fields = '__all__'
 
 
@@ -28,7 +29,31 @@ class LookupListFormTestCase(OpalTestCase):
             form.save()
 
     def test_valid_synonym(self):
-        hat = Hat.objects.create(name="Cowboy")
+        Hat.objects.create(name="Cowboy")
         form = HatForm()
         form.cleaned_data = dict(name="Stetson")
         self.assertEqual("Stetson", form.clean_name())
+
+
+class AdminTestCase(OpalTestCase):
+    def setUp(self):
+        self.patient, self.episode = self.new_patient_and_episode_please()
+        self.site = AdminSite()
+
+
+class EpisodeAdminTestCase(AdminTestCase):
+    def test_episode_detail_url(self):
+        admin = EpisodeAdmin(Episode, self.site)
+        self.assertEqual(
+            admin.episode_detail_url(self.episode),
+            "<a href='/#/patient/1/1'>/#/patient/1/1</a>"
+        )
+
+
+class PatientAdminTestCase(AdminTestCase):
+    def patient_detail_url(self):
+        admin = PatientAdmin(Patient, self.site)
+        self.assertEqual(
+            admin.patient_detail_url(self.patient),
+            "<a href='/#/patient/1'>/#/patient/1</a>"
+        )
