e2542895e79641cebc4d7c0822009764bdf1e61f
fredkingham <fredkingham@gmail.com>
2017-04-07 10:55:51 +0100

    withdraws a flat row for an episode subrecord

--- opal/core/search/extract.py
+++ opal/core/search/extract.py
@@ -10,6 +10,7 @@ import tempfile
 import zipfile
 from six import text_type
 from django.db.models import Count, Max
+from django.utils.functional import cached_property
 from opal.models import Episode
 from opal.core.subrecords import subrecords, episode_subrecords
 
@@ -204,12 +205,28 @@ class EpisodeSubrecordCsvRenderer(CsvRenderer):
             model, queryset, user, fields
         )
 
-    def get_flat_headers(self):
+    @cached_property
+    def repitions(self):
         e_values = self.queryset.values("episode_id")
         annotated = e_values.annotate(Count("episode_id"))
-        aggregated = annotated.aggregate(Max('episode_id__count')).values()[0]
+        return annotated.aggregate(Max('episode_id__count')).values()[0]
+
+    @cached_property
+    def row_length(self):
+        return len(self.get_headers()) * self.repitions
+
+    def get_flat_row_for_episode_id(self, episode_id):
+        by_episode_id = self.queryset.filter(episode_id=episode_id)
+        row = []
+        for instance in by_episode_id:
+            row.extend(self.get_row(instance))
+        if not len(row) == self.row_length:
+            row.extend("" for i in xrange(self.row_length - len(row)))
+        return row
+
+    def get_flat_headers(self):
         result = []
-        for i in xrange(aggregated):
+        for i in xrange(self.repitions):
             for header in self.get_headers():
                 result.append("{0}-{1} {2}".format(
                     self.model.get_display_name(),
