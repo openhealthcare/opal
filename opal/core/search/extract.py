bb04645579960455dbf411ce3f24af81b4a4f458
fredkingham <fredkingham@gmail.com>
2017-04-07 12:29:02 +0100

    fixes the patient subrecord extract

--- opal/core/search/extract.py
+++ opal/core/search/extract.py
@@ -9,6 +9,7 @@ import os
 import tempfile
 import zipfile
 from six import text_type
+from collections import defaultdict
 from opal.models import Episode
 from opal.core.subrecords import subrecords, episode_subrecords
 
@@ -158,9 +159,11 @@ class PatientSubrecordCsvRenderer(CsvRenderer):
     )
 
     def __init__(self, model, episode_queryset, user, fields=None):
-        self.patient_to_episode = {
-            e.patient_id: e.id for e in episode_queryset
-        }
+        self.patient_to_episode = defaultdict(list)
+
+        for episode in episode_queryset:
+            self.patient_to_episode[episode.patient_id].append(episode.id)
+
         queryset = model.objects.filter(
             patient__in=list(self.patient_to_episode.keys()))
 
@@ -177,7 +180,8 @@ class PatientSubrecordCsvRenderer(CsvRenderer):
 
     def get_rows(self):
         for sub in self.queryset:
-            yield self.get_row(sub, self.patient_to_episode[sub.patient_id])
+            for episode_id in self.patient_to_episode[sub.patient_id]:
+                yield self.get_row(sub, episode_id)
 
 
 class EpisodeSubrecordCsvRenderer(CsvRenderer):
