358ce05265a7239c96efe1771eb985c3d4bb8278
fredkingham <fredkingham@gmail.com>
2017-04-17 16:28:58 +0100

    allow the patient id to be passed in to the patient loader, add documentation

--- opal/static/js/opal/services/patient_loader.js
+++ opal/static/js/opal/services/patient_loader.js
@@ -1,29 +1,31 @@
 angular.module('opal.services').factory('patientLoader', function(
     $q, $window, $http, $route,
     recordLoader, FieldTranslater, Patient
-) {
-    return function() {
-        "use strict";
-	    var deferred = $q.defer();
-        var patient_id = $route.current.params.patient_id;
-        if(!patient_id){
-            deferred.resolve([]);
-            return deferred.promise;
-        }
-        var target = "/api/v0.1/patient/" + patient_id + '/';
-        var getPatientPromise = $http.get(target);
+){
+  return function(patientId) {
+    "use strict";
+    var deferred = $q.defer();
 
+    // if a patient id isn't passed in, defer to route params
+    patientId = patientId || $route.current.params.patient_id;
+    if(!patientId){
+      deferred.resolve([]);
+      return deferred.promise;
+    }
+    var target = "/api/v0.1/patient/" + patientId + '/';
+    var getPatientPromise = $http.get(target);
 
-        $q.all([recordLoader.load(), getPatientPromise]).then(
-            function(results)   {
-                var patient = new Patient(results[1].data);
-                deferred.resolve(patient);
-            },
-            function() {
-                // handle error better
-                $window.alert('Patient could not be loaded');
-                deferred.resolve();
-            });
-	    return deferred.promise;
-    };
+    $q.all([recordLoader.load(), getPatientPromise]).then(
+      function(results){
+        var patient = new Patient(results[1].data);
+        deferred.resolve(patient);
+      },
+      function(){
+        // handle error better
+        $window.alert('Patient could not be loaded');
+        deferred.resolve();
+    });
+
+    return deferred.promise;
+  };
 });
