9bef7d362006ef567d5412d16997bda3fcd2d15c
Marcus Baw <marcusbaw@gmail.com>
2017-12-04 18:15:26 +0000

    fixed line spacing to appease flake8

--- opal/core/pathway/context_processors.py
+++ opal/core/pathway/context_processors.py
@@ -1,13 +1,16 @@
 from opal.core.pathway import Pathway
 from django.utils.functional import SimpleLazyObject
 
+
 class PathwaysContextProcessor(object):
     def __init__(self):
         for i in Pathway.list():
             setattr(self, i.__name__, i)
 
+
 def hello(request):
     return {"hello": "there"}
+
 
 def pathways(request):
     return {
