be637ac34ef3d23306dcb869120b3d5ecf0b102f
David Miller <david@deadpansincerity.com>
2018-01-23 14:50:26 +0000

    Add core.referencedata package stub

---
+++ opal/core/referencedata/plugin.py
@@ -0,0 +1,9 @@
+"""
+Plugin definition for opal.core.referencedata
+"""
+from opal.core import plugins
+
+class ReferencedataPlugin(plugins.OpalPlugin):
+    """
+    The plugin entrypoint for core referencedata
+    """
