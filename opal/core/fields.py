f1563e0d02ccb4889d0ee8d50f2e188fe77dc5ea
David Miller <david@deadpansincerity.com>
2018-01-26 13:10:54 +0000

    Fixup: captitalization, typos etc

--- opal/core/fields.py
+++ opal/core/fields.py
@@ -81,21 +81,11 @@ class ForeignKeyOrFreeText(property):
         )
         ft_field.contribute_to_class(cls, self.ft_field_name)
 
-        # when we delete an instance in a lookup list, we want the
-        # value to be retained, even if though we've deleted the instance
-
-        # for example:
-        # if we have a condition, name='chest infection'
-        # we have diagnosis with fkft -> Condition
-        # then we have a patient with diagnosis.condition = 'Cough'
-        # at the db level this looks like
-        # then we have a patient who has
-        # diagnosis.condition = 'cough'
-        # ie diagnosis.condition_fk = Condition(name='cough')
-        # then we delete Condition(name='cough')
-        # we go through and change all diagnosis so that
-        # diagnosis.condition = 'cough' still
-        # but it wil be diagnosis.condition_ft = 'cough'
+        # When we delete an instance in a lookup list, we want the
+        # value to be retained, even though we've deleted the entry 
+        # in the lookuplist.
+        # Re-setting the value converts it to a free text entry of
+        # the same value as the original.
         def on_delete_cb(sender, instance, *args, **kwargs):
             if not cls._meta.abstract:
                 cls.objects.filter(**{
