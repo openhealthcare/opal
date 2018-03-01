4b332dbf42182a9dbdef7a3b50c4eb683a11ba10
David Miller <david@deadpansincerity.com>
2018-03-01 13:42:42 +0000

    Form and record views should allow for the fact that Python model names can have numbers in them

--- opal/tests/test-urls.py
+++
@@ -1,8 +0,0 @@
-from opal.core.test import OpalTestCase
-from django.core.urlresolvers import reverse
-
-
-class ReverseUrlTests(OpalTestCase):
-
-    def test_reverse_logout(self):
-        self.assertEqual('/accounts/logout/', reverse('logout'))
