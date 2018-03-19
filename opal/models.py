79fc1e6115a16589d08ae0fbae82fdacdfd1805f
David Miller <david@deadpansincerity.com>
2018-03-19 14:46:12 +0000

    Fixup: Whitespace

--- opal/models.py
+++ opal/models.py
@@ -1601,11 +1601,8 @@ class Investigation(EpisodeSubrecord):
     giardia               = models.CharField(max_length=20, blank=True)
     entamoeba_histolytica = models.CharField(max_length=20, blank=True)
     cryptosporidium       = models.CharField(max_length=20, blank=True)
-    rhinovirus = models.CharField(
-        max_length=20,
-        blank=True,
-        choices=POS_NEG_PENDING,
-    )
+    rhinovirus            = models.CharField(
+        max_length=20, blank=True, choices=POS_NEG_PENDING)
 
     class Meta:
         abstract = True
