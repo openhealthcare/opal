a0e4b912f677eed53c23a249c83db29d0fb76a9a
David Miller <david@deadpansincerity.com>
2018-06-21 15:52:00 +0100

    Add unittests for the load management command

--- opal/tests/test_command_load.py
+++ opal/tests/test_command_load.py
@@ -1,7 +1,7 @@
 """
 Unittests for import command
 """
-from mock import MagicMock
+from mock import MagicMock, mock_open, patch
 
 from opal.core.test import OpalTestCase
 
@@ -19,3 +19,16 @@ class LoadTestCase(OpalTestCase):
             help='Path to file containing a patient',
             default=None
         )
+
+    def test_handle_no_patient_file(self):
+        with self.assertRaises(ValueError):
+            c = load.Command()
+            c.handle()
+
+    def test_handle_reads_file(self):
+        c = load.Command()
+        m = mock_open(read_data='{"when": "Night and day"}')
+        with patch("__builtin__.open", m):
+            with patch.object(load.trade, 'import_patient') as importer:
+                c.handle(patient_file='data.json')
+                importer.assert_called_with({"when": "Night and day"})
