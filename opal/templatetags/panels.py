bf0a65157bee0a47d5c2715ba49f9e73764581d7
David Miller <david@deadpansincerity.com>
2019-11-15 13:03:54 +0000

    Make record panels work with patient subrecords

--- opal/templatetags/panels.py
+++ opal/templatetags/panels.py
@@ -19,16 +19,20 @@ def record_panel(context, model, only_display_if_exists=False):
     to be evaluated
     """
     context = copy.copy(context)
-    episode = context['episode']
+    patient = context['patient']
+    episode = None
     Model = model.__class__
+
     if issubclass(Model, PatientSubrecord):
-        records = Model.objects.filter(patient=episode.patient)
+        records = Model.objects.filter(patient=patient)
     else:
+        episode = context['episode']
         records = Model.objects.filter(episode=episode)
 
     ctx = {
         'model'                 : model,
         'episode'               : episode,
+        'patient'               : patient,
         'records'               : records,
         'singleton'             : getattr(model, '_is_singleton', False),
         'only_display_if_exists': only_display_if_exists,
