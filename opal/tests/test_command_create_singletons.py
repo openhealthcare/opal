5eccb62bbcaab14241a5fba2046e7e06d3959919
fredkingham <fredkingham@gmail.com>
2020-05-01 23:04:08 +0100

    create singleton was not actually logging, change to write to stdout and mock it in the tests

--- opal/tests/test_command_create_singletons.py
+++ opal/tests/test_command_create_singletons.py
@@ -1,11 +1,13 @@
 """
 Unittests for opal.management.commands.create_singletons
 """
+from unittest import mock
 from opal.core.test import OpalTestCase
 from opal.models import Patient, Episode
 from opal.tests.models import EpisodeName, FamousLastWords
 
 from opal.management.commands import create_singletons
+
 
 class CreateSingletonsTestCase(OpalTestCase):
 
@@ -14,13 +16,27 @@ class CreateSingletonsTestCase(OpalTestCase):
         FamousLastWords.objects.get(patient=p).delete()
         self.assertEqual(0, FamousLastWords.objects.filter(patient=p).count())
         c = create_singletons.Command()
-        c.handle()
+        with mock.patch.object(c.style, "SUCCESS") as success:
+            with mock.patch.object(c.stdout, "write") as stdout_write:
+                success.side_effect = lambda x: x
+                c.handle()
         self.assertEqual(1, FamousLastWords.objects.filter(patient=p).count())
+        stdout_write.assert_called_once_with("Created singletons: 1 FamousLastWords")
 
     def test_handle_create_episode_singleton(self):
         p, e = self.new_patient_and_episode_please()
         EpisodeName.objects.get(episode=e).delete()
         self.assertEqual(0, EpisodeName.objects.filter(episode=e).count())
         c = create_singletons.Command()
-        c.handle()
+        with mock.patch.object(c.style, "SUCCESS") as success:
+            with mock.patch.object(c.stdout, "write") as stdout_write:
+                success.side_effect = lambda x: x
+                c.handle()
         self.assertEqual(1, EpisodeName.objects.filter(episode=e).count())
+        stdout_write.assert_called_once_with("Created singletons: 1 EpisodeName")
+
+    def test_handle_with_none(self):
+        c = create_singletons.Command()
+        with mock.patch.object(c.stdout, "write") as stdout_write:
+            c.handle()
+        stdout_write.assert_called_once_with("No singletons needed to be created")
