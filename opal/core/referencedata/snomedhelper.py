d0d32ebabada423c2634e5055f3382f075eadb6c
JMathiszig-Lee <jakob@mathisziglee.co.uk>
2020-07-01 08:34:01 +0000

    used script to sketch out coding, writing something to do API calls going to take too long

--- opal/core/referencedata/snomedhelper.py
+++ opal/core/referencedata/snomedhelper.py
@@ -1,9 +1,20 @@
 import os
 import json
+import requests
 
+# def SearchSnomed(query:str, mode:str = 'partialMatching', results:int = 5 , semanticfilter:str = 'disorder' ):
+#     response = requests.get(
+#         'http://browser.ihtsdotools.org/api/v2/snomed/en-edition/v20180131/descriptions',
+#         params = {
+#             'query' : query,
+#             'searchMode': mode,
+#             'returnLimit': results,
+#             'semanticFilter': semanticfilter 
+#         }
+#     )
 
 LOOKUPLIST_LOCATION = os.path.join(
-    "data", "lookuplists", "condition.json"
+    "data", "lookuplists", "condition2.json"
 )
 
 with open(LOOKUPLIST_LOCATION, 'r') as fp:
@@ -15,4 +26,10 @@ for i in listcontent["condition"]:
         #coding already done
         pass
     else:
-        print(i["name"])
+        i["coding"] = {
+                "code": "0000000",
+                "system": "http://snomed.info/sct"
+            }
+
+with open(LOOKUPLIST_LOCATION, 'w') as fp:
+    json.dump(listcontent, fp, indent=2)
