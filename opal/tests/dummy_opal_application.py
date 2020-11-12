f10a2351bbb3733976fe264e220138be222bdeff
David Miller <david@deadpansincerity.com>
2020-11-12 16:44:29 +0000

    Episode category for tests and fix imports in tests

--- opal/tests/dummy_opal_application.py
+++ opal/tests/dummy_opal_application.py
@@ -4,6 +4,7 @@ Dummy OPAL application for running standalone tests
 from opal.core import application
 
 class Application(application.OpalApplication):
+    default_episode_category = 'Inpatient'
     pass
 
 list_schemas = {
