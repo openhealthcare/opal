124ab485cfc07d9659e1ecb63d802c8153b77947
fredkingham <fred.kingham@openhealthcare.org.uk>
2020-12-20 15:09:54 +0000

    bump the django version to 3.1.4

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     install_requires=[
         'ffs>=0.0.8.2',
         'Jinja2==2.10.1',
-        'django==3.0.11',
+        'django==3.1.4',
         'requests==2.25.0',
         'djangorestframework==3.12.2',
         'django-reversion==3.0.8',
