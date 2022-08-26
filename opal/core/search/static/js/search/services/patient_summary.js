1b7a3208277f56ddc77492d2a816ce80e2ca9589
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-08-26 08:51:13 +0100

    Updates the front end PatientSummary.js to include the full json payload
    Whatever is returned as a row from the search backend is put onto the
    patientSummary.
    
    This start_date and end_date are put onto the summary as startDate,
    endDate moments

--- opal/core/search/static/js/search/services/patient_summary.js
+++ opal/core/search/static/js/search/services/patient_summary.js
@@ -2,33 +2,45 @@
 // This is the main PatientSummary class for OPAL.
 //
 angular.module('opal.services').factory('PatientSummary', function(UserProfile) {
-        var PatientSummary = function(jsonResponse){
-            var startYear, endYear;
-            var self = this;
+    "use strict";
+    var PatientSummary = function(jsonResponse){
+        _.extend(this, jsonResponse);
+        var startYear, endYear
 
-            if(jsonResponse.start_date){
-                startYear= moment(jsonResponse.start_date, 'DD/MM/YYYY').format("YYYY");
-            }
+        if(jsonResponse.start_date){
+            this.startDate = moment(jsonResponse.start_date, 'DD/MM/YYYY');
+            startYear= this.start_date.format("YYYY");
+        }
 
-            if(jsonResponse.end_date){
-                endYear = moment(jsonResponse.end_date, 'DD/MM/YYYY').format("YYYY");
-            }
+        if(jsonResponse.end_date){
+            this.endDate = moment(jsonResponse.end_date, 'DD/MM/YYYY');
+            endYear = this.end_date.format("YYYY");
+        }
 
-            if(startYear && endYear && startYear !== endYear){
-              this.years = startYear + "-" + endYear;
-            }
-            else if(startYear){
-                this.years = startYear;
-            }
-            this.first_name = jsonResponse.first_name;
-            this.surname = jsonResponse.surname;
-            this.patientId = jsonResponse.patient_id;
-            this.count = jsonResponse.count;
+        if(startYear && endYear && startYear !== endYear){
+            this.years = startYear + "-" + endYear;
+        }
+        else if(startYear){
+            this.years = startYear;
+        }
+
+        if(jsonResponse.date_of_birth){
             this.dateOfBirth = moment(jsonResponse.date_of_birth, 'DD/MM/YYYY');
-            this.categories = jsonResponse.categories.join(", ");
-            this.link = "/#/patient/" + jsonResponse.patient_id;
-            this.hospitalNumber = jsonResponse.hospital_number;
-        };
+        }
 
-        return PatientSummary;
-    });
+        if(jsonResponse.patient_id){
+            this.link = "/#/patient/" + jsonResponse.patient_id;
+            this.patientId = jsonResponse.patient_id;
+        }
+
+        if(jsonResponse.categories){
+            this.categories = jsonResponse.categories.join(", ");
+        }
+
+        if(jsonResponse.hospital_number){
+            this.hospitalNumber = jsonResponse.hospital_number;
+        }
+    };
+
+    return PatientSummary;
+});
